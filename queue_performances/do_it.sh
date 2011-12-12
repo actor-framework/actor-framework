6dd260b6faa2c263f5b1fa4d25c6429ba4d1ddf2
neverlord <neverlord@neverbox.(none)>
2011-12-12 14:56:49 +0100

    test script

---
+++ queue_performances/do_it.sh
@@ -0,0 +1,50 @@
+echo 2 workers
+./queue_test 10000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 20000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 30000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 40000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 50000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 70000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 80000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 90000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 100000000 2 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 110000000 2 cached_stack "MSG_IN_MILLION TIME"
+
+echo
+echo 4 workers
+./queue_test 10000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 20000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 30000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 40000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 50000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 70000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 80000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 90000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 100000000 4 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 110000000 4 cached_stack "MSG_IN_MILLION TIME"
+
+echo
+echo 8 workers
+./queue_test 10000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 20000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 30000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 40000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 50000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 70000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 80000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 90000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 100000000 8 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 110000000 8 cached_stack "MSG_IN_MILLION TIME"
+
+echo
+echo 16 workers
+./queue_test 10000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 20000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 30000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 40000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 50000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 70000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 80000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 90000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 100000000 16 cached_stack "MSG_IN_MILLION TIME"
+./queue_test 110000000 16 cached_stack "MSG_IN_MILLION TIME"
