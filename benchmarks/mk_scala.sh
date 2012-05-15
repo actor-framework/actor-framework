261be130ef12db59ca73981723bde575b3da73b9
Dominik Charousset <neverlord@mobi20.cpt.haw-hamburg.de>
2012-05-15 15:55:55 +0200

    show more warnings

--- benchmarks/mk_scala.sh
+++ benchmarks/mk_scala.sh
@@ -2,10 +2,10 @@
 if [[ $# -eq 0 ]] ; then
     for i in *.scala; do
 	    echo "compile \"$i\""
-	    scalac -cp $AKKA_LIBS "$i"
+	    scalac -unchecked -cp $AKKA_LIBS "$i"
     done
 elif [[ $# -eq 1 ]] ; then
     echo "compile \"$1.scala\""
-    scalac -cp $AKKA_LIBS "$1.scala"
+    scalac -unchecked -cp $AKKA_LIBS "$1.scala"
 fi
 echo done
