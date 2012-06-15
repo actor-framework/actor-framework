f5767cc0ca454a32cd4dcc7b8d31c0caa017a485
Dominik Charousset <neverlord@mobi20.cpt.haw-hamburg.de>
2012-06-15 09:41:13 +0200

    documentation

--- cppa/cppa.hpp
+++ cppa/cppa.hpp
@@ -71,40 +71,36 @@
  *
  * This library provides an implementation of the actor model for C++.
  * It uses a network transparent messaging system to ease development
- * of both concurrent and distributed software using C++.
+ * of both concurrent and distributed software.
  *
  * @p libcppa uses a thread pool to schedule actors by default.
  * A scheduled actor should not call blocking functions.
  * Individual actors can be spawned (created) with a special flag to run in
  * an own thread if one needs to make use of blocking APIs.
  *
- * Writing applications in @p libcppa requires a minimum of gluecode.
- * You don't have to derive a particular class to implement an actor and
+ * Writing applications in @p libcppa requires a minimum of gluecode and
  * each context <i>is</i> an actor. Even main is implicitly
- * converted to an actor if needed, as the following example shows:
- *
- * It's recommended to read at least the
- * {@link MessageHandling message handling}
- * section of this documentation.
+ * converted to an actor if needed.
  *
  * @section GettingStarted Getting Started
  *
- * To build @p libcppa, you need <tt>GCC >= 4.6</tt>, @p Automake
- * and the <tt>Boost.Thread</tt> library.
+ * To build @p libcppa, you need <tt>GCC >= 4.7</tt> or <tt>Clang >= 3.2</tt>,
+ * @p CMake and the <tt>Boost.Thread</tt> library.
  *
  * The usual build steps on Linux and Mac OS X are:
  *
- * - <tt>autoreconf -i</tt>
- * - <tt>./configure</tt>
- * - <tt>make</tt>
+ * - <tt>mkdir build</tt>
+ * - <tt>cd build</tt>
+ * - <tt>cmake ..</tt>
+ * - <tt>make</tt> (as root, optionally)
  * - <tt>make install</tt> (as root, optionally)
- *
- * Use <tt>./configure --help</tt> if the script doesn't auto-select
- * the correct @p GCC binary or doesn't find your <tt>Boost.Thread</tt>
- * installation.
  *
  * Windows is not supported yet, because MVSC++ doesn't implement the
  * C++11 features needed to compile @p libcppa.
+ *
+ * Please read the <b>Manual</b> for an introduction to @p libcppa.
+ * It is available online at
+ * http://neverlord.github.com/libcppa/manual/
  *
  * @section IntroHelloWorld Hello World Example
  *
