cd2f32fef55fedc065e10f0ff69e7111c9c4766e
Dominik Charousset <neverlord@mobi20.cpt.haw-hamburg.de>
2012-07-25 19:52:32 +0200

    update manual as well

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-doxygen libcppa.doxygen
-cd ../gh-pages
-rm -f *.tex *.html *.css *.png *.js
+echo "build documentation ..."
+make doc &>/dev/null
+
+cd ../inetd/libcppa_manual/
+if [ -f manual.pdf ]; then
+    echo "PDF manual found ..."
+else
+    echo "no PDF manual found ... stop"
+    exit
+fi
+
+echo "build HTML manual ..."
+# run hevea three times
+for i in {1..3} ; do
+    hevea manual.tex &>/dev/null 
+done
+
+echo "copy documentation into gh-pages ..."
+cd ../../gh-pages
+rm -rf *.tex *.html *.css *.png *.js manual
 cp -R ../libcppa/html/* .
+mkdir manual
+cp ../inetd/libcppa_manual/manual.pdf manual/
+cp ../inetd/libcppa_manual/manual.html manual/index.html
+
+echo "commit ..."
 git add .
 git commit -a -m "documentation update"
+
+echo "push ..."
 git push
+
 cd ../libcppa
