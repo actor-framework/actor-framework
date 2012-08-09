ab34ff1bb670b7a3577ae1994a77fec1e6d824dd
Dominik Charousset <neverlord@mobi20.cpt.haw-hamburg.de>
2012-08-09 13:37:36 +0200

    moved behavior definition of ping & pong to factory functions

--- unit_testing/ping_pong.cpp
+++ unit_testing/ping_pong.cpp
@@ -6,11 +6,47 @@
 #include "cppa/sb_actor.hpp"
 #include "cppa/to_string.hpp"
 
-namespace { size_t s_pongs = 0; }
-
 using std::cout;
 using std::endl;
 using namespace cppa;
+
+namespace {
+
+size_t s_pongs = 0;
+
+behavior ping_behavior(size_t num_pings) {
+    return  on(atom("pong"), arg_match) >> [num_pings](int value) {
+                //cout << to_string(self->last_dequeued()) << endl;
+                if (++s_pongs >= num_pings) {
+                    reply(atom("EXIT"), exit_reason::user_defined);
+                    self->quit();
+                }
+                else {
+                    reply(atom("ping"), value);
+                }
+            },
+            others() >> [] {
+                cout << "unexpected message; "
+                     << __FILE__ << " line " << __LINE__ << ": "
+                     << to_string(self->last_dequeued()) << endl;
+                self->quit(exit_reason::user_defined);
+            };
+}
+
+behavior pong_behavior() {
+    return  on<atom("ping"), int>() >> [](int value) {
+                //cout << to_string(self->last_dequeued()) << endl;
+                reply(atom("pong"), value + 1);
+            },
+            others() >> []() {
+                cout << "unexpected message; "
+                     << __FILE__ << " line " << __LINE__ << ": "
+                     << to_string(self->last_dequeued()) << endl;
+                self->quit(exit_reason::user_defined);
+            };
+}
+
+} // namespace <anonymous>
 
 size_t pongs() {
     return s_pongs;
@@ -18,92 +54,28 @@ size_t pongs() {
 
 void ping(size_t num_pings) {
     s_pongs = 0;
-    do_receive (
-        on<atom("pong"), int>() >> [&](int value) {
-            //cout << to_string(self->last_dequeued()) << endl;
-            if (++s_pongs == num_pings) {
-                reply(atom("EXIT"), exit_reason::user_defined);
-            }
-            else {
-                reply(atom("ping"), value);
-            }
-        },
-        others() >> []() {
-            cout << "unexpected message; "
-                 << __FILE__ << " line " << __LINE__ << ": "
-                 << to_string(self->last_dequeued()) << endl;
-            self->quit(exit_reason::user_defined);
-        }
-    )
-    .until(gref(s_pongs) == num_pings);
+    receive_loop(ping_behavior(num_pings));
 }
 
 actor_ptr spawn_event_based_ping(size_t num_pings) {
     s_pongs = 0;
-    struct impl : public sb_actor<impl> {
-        behavior init_state;
-        impl(size_t num_pings) {
-            init_state = (
-                on<atom("pong"), int>() >> [num_pings, this](int value) {
-                    //cout << to_string(self->last_dequeued()) << endl;
-                    if (++s_pongs >= num_pings) {
-                        reply(atom("EXIT"), exit_reason::user_defined);
-                        quit();
-                    }
-                    else {
-                        reply(atom("ping"), value);
-                    }
-                },
-                others() >> []() {
-                    cout << "unexpected message; "
-                         << __FILE__ << " line " << __LINE__ << ": "
-                         << to_string(self->last_dequeued()) << endl;
-                    self->quit(exit_reason::user_defined);
-                }
-            );
+    return factory::event_based(
+        [num_pings] {
+            self->become(ping_behavior(num_pings));
         }
-    };
-    return spawn<impl>(num_pings);
+    ).spawn();
 }
 
 void pong(actor_ptr ping_actor) {
     // kickoff
     send(ping_actor, atom("pong"), 0);
-    receive_loop (
-        on<atom("ping"), int>() >> [](int value) {
-            //cout << to_string(self->last_dequeued()) << endl;
-            reply(atom("pong"), value + 1);
-        },
-        others() >> []() {
-            cout << "unexpected message; "
-                 << __FILE__ << " line " << __LINE__ << ": "
-                 << to_string(self->last_dequeued()) << endl;
-            self->quit(exit_reason::user_defined);
-        }
-    );
+    receive_loop (pong_behavior());
 }
 
 actor_ptr spawn_event_based_pong(actor_ptr ping_actor) {
     CPPA_REQUIRE(ping_actor.get() != nullptr);
-    struct impl : public sb_actor<impl> {
-        behavior init_state;
-        impl() {
-            init_state = (
-                on<atom("ping"), int>() >> [](int value) {
-                    //cout << to_string(self->last_dequeued()) << endl;
-                    reply(atom("pong"), value + 1);
-                },
-                others() >> []() {
-                    cout << "unexpected message; "
-                         << __FILE__ << " line " << __LINE__ << ": "
-                         << to_string(self->last_dequeued()) << endl;
-                    self->quit(exit_reason::user_defined);
-                }
-            );
-        }
-    };
-    auto pptr = spawn<impl>();
-    // kickoff
-    ping_actor->enqueue(pptr.get(), make_any_tuple(atom("pong"), 0));
-    return pptr;
+    return  factory::event_based([=] {
+                self->become(pong_behavior());
+                send(ping_actor, atom("pong"), 0);
+            }).spawn();
 }
