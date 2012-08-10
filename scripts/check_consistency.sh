ec47448e5f66d6a6a170d402fe472029e9b39109
Dominik Charousset <neverlord@mobi20.cpt.haw-hamburg.de>
2012-08-10 11:23:55 +0200

    added check for docu version

--- scripts/check_consistency.sh
+++ scripts/check_consistency.sh
@@ -9,23 +9,34 @@ if [ ! -d manual ]; then
     exit
 fi
 
-make -C manual &>/dev/null
+echo "build manual/manual.pdf"
+make -C manual pdf &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "failure during build!"
+    exit
+fi
+echo "read manual.pdf"
 PDF1=$(pdf2txt.py manual.pdf)
+echo "read manual/manual.pdf"
 PDF2=$(pdf2txt.py manual/manual.pdf)
 if [ "$PDF1" != "$PDF2" ] ; then
     echo "manual.pdf and manual/manual.pdf differ!"
     exit
+else
+    echo "manual.pdf up-to-date"
 fi
 
 CMAKE_VERSION=$(grep -oE "set\(LIBCPPA_VERSION_(MAJOR|MINOR|PATCH) [0-9]+" CMakeLists.txt | awk '{ if (NR > 1) printf "." ; printf $2 } END { printf "\n" }')
 MANUAL_VERSION=$(echo "$PDF1" | grep -oE "version [0-9]+(\.[0-9]+){2}" | awk '{print $2}')
 CHANGELOG_VERSION=$(head -n1 ChangeLog | awk '{print $2}')
+DOCU_VERSION=$(grep -oE "Version [0-9]+(\.[0-9]+){2}" Doxyfile.in  | awk '{print $2}')
 
 echo "libcppa version in CMakeLists.txt is $CMAKE_VERSION"
 echo "libcppa version in manual.pdf is $MANUAL_VERSION"
 echo "libcppa version in ChangeLog is $CHANGELOG_VERSION"
+echo "libcppa version in documentation is $DOCU_VERSION"
 
-if [ "$CMAKE_VERSION" == "$MANUAL_VERSION" ] && [ "$CMAKE_VERSION" == "$CHANGELOG_VERSION" ]; then
+if [ "$CMAKE_VERSION" == "$MANUAL_VERSION" ] && [ "$CMAKE_VERSION" == "$CHANGELOG_VERSION" ] && [ "$CMAKE_VERSION" == "$DOCU_VERSION" ]; then
     echo "no errors found"
 else
     echo "versions differ"
