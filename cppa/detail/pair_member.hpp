4a0c6bd2f9f45fa86e1dd2390875683411be1106
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2012-09-05 20:19:23 +0200

    allow pairs to use all builtin types rather than primitive types only

--- cppa/detail/pair_member.hpp
+++ cppa/detail/pair_member.hpp
@@ -36,23 +36,23 @@
 #include "cppa/serializer.hpp"
 #include "cppa/deserializer.hpp"
 #include "cppa/primitive_variant.hpp"
-#include "cppa/detail/type_to_ptype.hpp"
+
+#include "cppa/util/is_builtin.hpp"
+#include "cppa/util/is_primitive.hpp"
 #include "cppa/util/abstract_uniform_type_info.hpp"
+
+#include "cppa/detail/types_array.hpp"
+#include "cppa/detail/type_to_ptype.hpp"
 
 namespace cppa { namespace detail {
 
-template<typename T1, typename T2>
-class pair_member : public util::abstract_uniform_type_info<std::pair<T1,T2>> {
+template<typename T1, typename T2, bool PrimitiveTypes> // default: true
+struct pair_member_impl {
 
     static constexpr primitive_type ptype1 = type_to_ptype<T1>::ptype;
     static constexpr primitive_type ptype2 = type_to_ptype<T2>::ptype;
 
-    static_assert(ptype1 != pt_null, "T1 is not a primitive type");
-    static_assert(ptype2 != pt_null, "T2 is not a primitive type");
-
     typedef std::pair<T1, T2> pair_type;
-
- public:
 
     void serialize(const void* obj, serializer* s) const {
         auto& p = *reinterpret_cast<const pair_type*>(obj);
@@ -68,6 +68,51 @@ class pair_member : public util::abstract_uniform_type_info<std::pair<T1,T2>> {
         p.first = std::move(get<T1>(values[0]));
         p.second = std::move(get<T2>(values[1]));
     }
+
+};
+
+template<typename T1, typename T2>
+struct pair_member_impl<T1, T2, false> {
+
+    typedef std::pair<T1, T2> pair_type;
+
+    void serialize(const void* obj, serializer* s) const {
+        auto& arr = static_types_array<T1, T2>::arr;
+        auto& p = *reinterpret_cast<const pair_type*>(obj);
+        arr[0]->serialize(&p.first, s);
+        arr[1]->serialize(&p.second, s);
+    }
+
+    void deserialize(void* obj, deserializer* d) const {
+        auto& arr = static_types_array<T1, T2>::arr;
+        auto& p = *reinterpret_cast<pair_type*>(obj);
+        arr[0]->deserialize(&p.first, d);
+        arr[1]->deserialize(&p.second, d);
+    }
+
+};
+
+template<typename T1, typename T2>
+class pair_member : public util::abstract_uniform_type_info<std::pair<T1,T2>> {
+
+    static_assert(util::is_builtin<T1>::value, "T1 is not a builtin type");
+    static_assert(util::is_builtin<T1>::value, "T2 is not a builtin type");
+
+ public:
+
+    void serialize(const void* obj, serializer* s) const {
+        impl.serialize(obj, s);
+    }
+
+    void deserialize(void* obj, deserializer* d) const {
+        impl.deserialize(obj, d);
+    }
+
+ private:
+
+    pair_member_impl<T1, T2,    util::is_primitive<T1>::value
+                             && util::is_primitive<T2>::value> impl;
+
 
 };
 
