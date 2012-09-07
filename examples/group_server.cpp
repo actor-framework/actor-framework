d5ec1025872d362b43f86d46a743110e1dd37913
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2012-09-07 19:48:58 +0200

    polish chat_client and group_server up

--- examples/group_server.cpp
+++ examples/group_server.cpp
@@ -5,39 +5,20 @@
 
 #include "cppa/opt.hpp"
 #include "cppa/cppa.hpp"
+#include "type_plugins.hpp"
 
 using namespace std;
 using namespace cppa;
 
 int main(int argc, char** argv) {
-
-    // user-defined types can be announced by a plugin
-    void* handle = dlopen("plugin.dylib", RTLD_NOW);     // macos
-    if (!handle) handle = dlopen("plugin.so", RTLD_NOW); // linux
-    if (handle) {
-        auto before = uniform_type_info::instances();
-        cout << "found a plugin, call exec_plugin()" << endl;
-        auto fun = (void (*)()) dlsym(handle, "exec_plugin");
-        if (fun) {
-            fun();
-            cout << "the plugin announced the following types:" << endl;
-            for (auto inf : uniform_type_info::instances()) {
-                if (count(begin(before), end(before), inf) == 0) {
-                    cout << inf->name() << endl;
-                }
-            }
-        }
-    }
-
+    // enables this server to be used with user-defined types
+    // without changing this source code
+    exec_plugin();
     uint16_t port = 0;
+    options_description desc;
     bool args_valid = argc > 1 && match_stream<string>(argv + 1, argv + argc) (
-        on_opt('p', "port") >> [&](const string& arg) -> bool {
-            if (!(istringstream(arg) >> port)) {
-                cout << "\"" << arg << "\" is not a valid port" << endl;
-                return false;
-            }
-            return true;
-        }
+        on_opt('p', "port", &desc, "set port") >> rd_arg(port),
+        on_vopt('h', "help", &desc, "print help") >> print_desc_and_exit(&desc)
     );
     if (port <= 1024) {
         cout << "no port > 1024 given" << endl;
@@ -49,9 +30,8 @@ int main(int argc, char** argv) {
     string line;
     while (getline(cin, line)) {
         if (line == "quit") return 0;
-        else {
-            cout << "illegal command" << endl;
-        }
+        else cout << "illegal command" << endl;
     }
+    shutdown();
     return 0;
 }
