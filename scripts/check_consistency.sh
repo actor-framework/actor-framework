6e3b85f0937162c0cc80b5c5c11126fc57d9f0d8
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2012-10-01 17:25:33 +0200

    changed version to 0.4.2

--- scripts/check_consistency.sh
+++ scripts/check_consistency.sh
@@ -32,7 +32,7 @@ function expand_version_string {
 
 CMAKE_VERSION=$(grep -oE "set\(LIBCPPA_VERSION_(MAJOR|MINOR|PATCH) [0-9]+" CMakeLists.txt | awk '{ if (NR > 1) printf "." ; printf $2 } END { printf "\n" }')
 MANUAL_VERSION=$(echo "$PDF1" | grep -oE "version [0-9]+(\.[0-9]+){1,2}" | awk '{print $2}')
-CHANGELOG_VERSION=$(head -n1 ChangeLog | awk '{print $2}')
+CHANGELOG_VERSION=$(head -n1 ChangeLog.md | awk '{print $2}')
 DOCU_VERSION=$(grep -oE "Version [0-9]+(\.[0-9]+){1,2}" Doxyfile.in  | awk '{print $2}')
 
 MANUAL_VERSION=$(expand_version_string "$MANUAL_VERSION")
