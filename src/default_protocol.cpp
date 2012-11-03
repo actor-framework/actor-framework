6d860687f58ce157a41df38c43dce267c24a4da9
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2012-11-03 23:58:37 +0100

    fixed log output in default protocol

--- src/default_protocol.cpp
+++ src/default_protocol.cpp
@@ -170,7 +170,7 @@ struct remote_actor_result { remote_actor_result* next; actor_ptr value; };
 actor_ptr default_protocol::remote_actor(io_stream_ptr_pair io,
                                          variant_args args         ) {
     CPPA_LOG_TRACE("io = {" << io.first.get() << ", " << io.second.get() << "}, "
-                   << " << args.size() = " << args.size());
+                   << "args.size() = " << args.size());
     CPPA_REQUIRE(args.size() == 0);
     static_cast<void>(args); // keep compiler happy when compiling w/o debug
     auto pinf = process_information::get();
