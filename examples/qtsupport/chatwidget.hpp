6bad5f9837bbb8fed72e8341e0b988292f13d78c
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2012-11-07 00:10:13 +0100

    Qt example highlighting actor_widget_mixin
    this example optionally builds when Qt4 is found during CMake
    invokation and illustrates how to use actor_widget_mixin to send
    and receive messages from a QWidget-based class

---
+++ examples/qtsupport/chatwidget.hpp
@@ -0,0 +1,56 @@
+#include <exception>
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QTextEdit>
+
+#include "cppa/group.hpp"
+#include "cppa/qtsupport/actor_widget_mixin.hpp"
+
+class ChatWidget : public cppa::actor_widget_mixin<QWidget> {
+
+    Q_OBJECT
+
+    typedef cppa::actor_widget_mixin<QWidget> super;
+
+ public:
+
+    ChatWidget(QWidget* parent = nullptr, Qt::WindowFlags f = 0);
+
+ public slots:
+
+    void sendChatMessage();
+    void joinGroup();
+    void changeName();
+
+ private:
+
+    template<typename T>
+    T* get(T*& member, const char* name) {
+        if (member == nullptr) {
+            member = findChild<T*>(name);
+            if (member == nullptr)
+                throw std::runtime_error("unable to find child: "
+                                         + std::string(name));
+        }
+        return member;
+    }
+
+    inline QLineEdit* input() {
+        return get(m_input, "input");
+    }
+
+    inline QTextEdit* output() {
+        return get(m_output, "output");
+    }
+
+    inline void print(const QString& what) {
+        output()->append(what);
+    }
+
+    QLineEdit* m_input;
+    QTextEdit* m_output;
+    std::string m_name;
+    cppa::group_ptr m_chatroom;
+
+};
---
+++ examples/qtsupport/group_chat.cpp
@@ -0,0 +1,102 @@
+/******************************************************************************\
+ * This example program represents a minimal terminal chat program            *
+ * based on group communication.                                              *
+ *                                                                            *
+ * Setup for a minimal chat between "alice" and "bob":                        *
+ * - ./build/bin/group_server -p 4242                                         *
+ * - ./build/bin/group_chat -g remote:chatroom@localhost:4242 -n alice        *
+ * - ./build/bin/group_chat -g remote:chatroom@localhost:4242 -n bob          *
+\******************************************************************************/
+
+#include <set>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <cstdlib>
+
+#include <QMainWindow>
+#include <QApplication>
+
+#include "cppa/opt.hpp"
+#include "cppa/cppa.hpp"
+
+#include "ui_chatwindow.h" // auto generated from chatwindow.ui
+
+using namespace std;
+using namespace cppa;
+using namespace cppa::placeholders;
+
+struct line { string str; };
+
+istream& operator>>(istream& is, line& l) {
+    getline(is, l.str);
+    return is;
+}
+
+any_tuple s_last_line;
+
+any_tuple split_line(const line& l) {
+    vector<string> result;
+    stringstream strs(l.str);
+    string tmp;
+    while (getline(strs, tmp, ' ')) {
+        if (!tmp.empty()) result.push_back(std::move(tmp));
+    }
+    s_last_line = any_tuple::view(std::move(result));
+    return s_last_line;
+}
+
+int main(int argc, char** argv) {
+
+    string name;
+    string group_id;
+    options_description desc;
+    bool args_valid = match_stream<string>(argv + 1, argv + argc) (
+        on_opt1('n', "name", &desc, "set name") >> rd_arg(name),
+        on_opt1('g', "group", &desc, "join group <arg1>") >> rd_arg(group_id),
+        on_opt0('h', "help", &desc, "print help") >> print_desc_and_exit(&desc)
+    );
+
+    group_ptr gptr;
+    // evaluate group parameter
+    if (!group_id.empty()) {
+        auto p = group_id.find(':');
+        if (p == std::string::npos) {
+            cerr << "*** error parsing argument " << group_id
+                 << ", expected format: <module_name>:<group_id>";
+        }
+        else {
+            try {
+                gptr = group::get(group_id.substr(0, p),
+                                  group_id.substr(p + 1));
+            }
+            catch (exception& e) {
+                ostringstream err;
+                cerr << "*** exception: group::get(\"" << group_id.substr(0, p)
+                     << "\", \"" << group_id.substr(p + 1) << "\") failed; "
+                     << to_verbose_string(e) << endl;
+            }
+        }
+    }
+
+    if (!args_valid) print_desc_and_exit(&desc)();
+
+    QApplication app(argc, argv);
+    app.setQuitOnLastWindowClosed(true);
+    QMainWindow mw;
+    Ui::ChatWindow helper;
+    helper.setupUi(&mw);
+    auto client = helper.chatwidget->as_actor();
+    if (!name.empty()) {
+        send_as(client, client, atom("setName"), move(name));
+    }
+    if (gptr != nullptr) {
+        send_as(client, client, atom("join"), gptr);
+    }
+    mw.show();
+    return app.exec();
+    shutdown();
+    return 0;
+}
