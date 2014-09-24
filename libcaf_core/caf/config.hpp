ebe66b02898226d539644da91ddd20799f6b62c0
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-09-24 16:05:07 +0200

    Change version to 0.11

--- libcaf_core/caf/config.hpp
+++ libcaf_core/caf/config.hpp
@@ -37,7 +37,7 @@
  * whereas each number is a two-digit decimal number without
  * leading zeros (e.g. 900 is version 0.9.0).
  */
-#define CAF_VERSION 1002
+#define CAF_VERSION 1100
 
 #define CAF_MAJOR_VERSION (CAF_VERSION / 10000)
 #define CAF_MINOR_VERSION ((CAF_VERSION / 100) % 100)
