667a5950b82b7f6ac408a27176cb84a6ca6147b0
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2015-03-02 15:04:22 +0100

    Discard data on disconnected scribes

--- libcaf_io/src/broker.cpp
+++ libcaf_io/src/broker.cpp
@@ -84,6 +84,13 @@ message broker::scribe::disconnect_message() {
 
 void broker::scribe::consume(const void*, size_t num_bytes) {
   CAF_LOG_TRACE(CAF_ARG(num_bytes));
+  if (m_disconnected) {
+    // we are already disconnected from the broker while the multiplexer
+    // did not yet remove the socket, this can happen if an IO event causes
+    // the broker to call close_all() while the pollset contained
+    // further activities for the broker
+    return;
+  }
   auto& buf = rd_buf();
   buf.resize(num_bytes);                       // make sure size is correct
   read_msg().buf.swap(buf);                    // swap into message
