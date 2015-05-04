c23ac61e6461894e61af210d31c593eff9fea9aa
Joseph Noir <joseph.noir@gmail.com>
2015-05-04 18:49:17 +0200

    Add header to find module with FindLibcaf

---
+++ libcaf_opencl/caf/opencl/all.hpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright (C) 2011 - 2015                                                  *
+ * Dominik Charousset <dominik.charousset (at) haw-hamburg.de>                *
+ * Raphael Hiesgen <raphael.hiesgen (at) haw-hamburg.de>                      *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#ifndef CAF_OPENCL_ALL_HPP
+#define CAF_OPENCL_ALL_HPP
+
+#include "caf/opencl/spawn_cl.hpp"
+
+#endif // CAF_OPENCL_ALL_HPP
