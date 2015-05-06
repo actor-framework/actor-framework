7a7870f1707a10e0e6c20dfb071339c617a9fffc
ufownl <ufownl@gmail.com>
2015-05-06 13:14:54 +0800

    Resume the correct behavior when on_exit did set a new behavior.

--- libcaf_core/src/local_actor.cpp
+++ libcaf_core/src/local_actor.cpp
@@ -583,15 +583,23 @@ resumable::resume_result local_actor::resume(execution_unit* eu,
     CAF_LOG_TRACE("");
     bhvr_stack().clear();
     bhvr_stack().cleanup();
+    auto responses = std::move(m_pending_responses);
     on_exit();
     if (has_behavior()) {
       CAF_LOG_DEBUG("on_exit did set a new behavior");
-      // if the actor still has behaviors, we should record the current exit reason
-      if (planned_exit_reason() != exit_reason::not_exited) {
-        m_last_exit_reason = planned_exit_reason();
-      }
+      m_last_exit_reason = exit_reason::normal;
       planned_exit_reason(exit_reason::not_exited);
       return false; // on_exit did set a new behavior
+    } else if (!responses.empty()) {
+      // the actor still has pending responses and on_exit didn't set a new
+      // behavior, we should record the current exit reason
+      auto rsn = planned_exit_reason();
+      if (rsn != exit_reason::not_exited) {
+        m_last_exit_reason = rsn;
+      }
+      m_pending_responses = std::move(responses);
+      planned_exit_reason(exit_reason::not_exited);
+      return false;
     }
     auto rsn = planned_exit_reason();
     if (rsn == exit_reason::not_exited) {
