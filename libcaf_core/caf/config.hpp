8200b7034b4e8a2eacdee6e48c4f41b2d1ae5905
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2015-06-23 15:34:54 +0200

    Change version to 0.14

--- libcaf_core/caf/config.hpp
+++ libcaf_core/caf/config.hpp
@@ -32,7 +32,7 @@
 /// Denotes version of CAF in the format {MAJOR}{MINOR}{PATCH},
 /// whereas each number is a two-digit decimal number without
 /// leading zeros (e.g. 900 is version 0.9.0).
-#define CAF_VERSION 1302
+#define CAF_VERSION 1400
 
 /// Defined to the major version number of CAF.
 ///*/
