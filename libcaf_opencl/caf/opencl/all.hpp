SteveShaw/opencl

Adapt to actor_system update
The metainfo is now a module and can be loaded by an actor_system.

caf/opencl/all.hpp
@@ -21,6 +21,6 @@
 #ifndef CAF_OPENCL_ALL_HPP
 #define CAF_OPENCL_ALL_HPP
 
-#include "caf/opencl/spawn_cl.hpp"
+#include "caf/opencl/metainfo.hpp"
 
 #endif // CAF_OPENCL_ALL_HPP

caf/opencl/device.hpp
@@ -29,9 +29,11 @@ namespace caf {
 namespace opencl {
 
 class program;
+class metainfo;
 
 class device {
   friend class program;
+  friend class metainfo;
 
 public:
   /// Intialize a new device in a context using a sepcific device_id

actor-framework/actor-framework

libcaf_opencl/caf/opencl/all.hpp
@@ -21,6 +21,6 @@
 #ifndef CAF_OPENCL_ALL_HPP
 #define CAF_OPENCL_ALL_HPP
 
-#include "caf/opencl/spawn_cl.hpp"
+#include "caf/opencl/manager.hpp"
 
 #endif // CAF_OPENCL_ALL_HPP

libcaf_opencl/caf/opencl/device.hpp
@@ -29,11 +29,13 @@ namespace caf {
 namespace opencl {
 
 class program;
+class manager;
 
 class device {
-  friend class program;
-
 public:
+  friend class program;
+  friend class manager;
+
   /// Intialize a new device in a context using a sepcific device_id
   static device create(context_ptr context, device_ptr device_id, unsigned id);
   /// Get the id assigned by caf