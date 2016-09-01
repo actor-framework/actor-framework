d57b3c4fad103aabb3fd08de0372a846daa80ff9
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2016-09-01 09:08:48 +0200

    Update copyright

--- libcaf_core/src/message_handler.cpp
+++ libcaf_core/src/message_handler.cpp
@@ -5,7 +5,7 @@
  *                     | |___ / ___ \|  _|      Framework                     *
  *                      \____/_/   \_|_|                                      *
  *                                                                            *
- * Copyright (C) 2011 - 2015                                                  *
+ * Copyright (C) 2011 - 2016                                                  *
  * Dominik Charousset <dominik.charousset (at) haw-hamburg.de>                *
  *                                                                            *
  * Distributed under the terms and conditions of the BSD 3-Clause License or  *
