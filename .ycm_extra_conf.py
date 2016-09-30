b941558433333f9010274e1dd906be020a7cac50
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2016-09-30 09:45:36 +0200

    Remove seemingly broken YCM option

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -28,7 +28,7 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = 'build'
+compilation_database_folder = ''
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
