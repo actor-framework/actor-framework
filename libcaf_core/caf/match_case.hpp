fc6c187eceb4574ecc76e1a05b0a35d6ea560415
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2016-11-14 16:39:30 +0100

    Remove outdated code

--- libcaf_core/caf/match_case.hpp
+++ libcaf_core/caf/match_case.hpp
@@ -127,16 +127,6 @@ public:
       param_decay
     >::type;
 
-  /*
-  static_assert(!std::is_same<pattern, detail::type_list<exit_msg>>::value,
-                "exit_msg not allowed in message handlers, "
-                "did you mean to use set_exit_handler()?");
-
-  static_assert(!std::is_same<pattern, detail::type_list<down_msg>>::value,
-                "down_msg not allowed in message handlers, "
-                "did you mean to use set_down_handler()?");
-  */
-
   using decayed_arg_types =
     typename detail::tl_map<
       arg_types,
