b21c023515665839d269b942b2a820d3e57092a8
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2017-03-06 17:25:04 +0100

    Fix handshake in incoming_stream_multiplexer

--- libcaf_core/src/incoming_stream_multiplexer.cpp
+++ libcaf_core/src/incoming_stream_multiplexer.cpp
@@ -67,7 +67,7 @@ void incoming_stream_multiplexer::operator()(stream_msg::open& x) {
   auto ptr = make_mailbox_element(
     cme->sender, cme->mid, std::move(cme->stages),
     make<stream_msg::open>(current_stream_msg_->sid, std::move(x.msg),
-                           self_->ctrl(), successor, x.priority,
+                           self_->ctrl(), x.original_stage, x.priority,
                            x.redeployable));
   successor->enqueue(std::move(ptr), self_->context());
   // Send out demand upstream.
