c2547620bb67f6c8939f510585c96759b2aedf05
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2017-06-30 10:59:28 +0200

    Fix bug in actor_config's to_string

--- libcaf_core/src/actor_config.cpp
+++ libcaf_core/src/actor_config.cpp
@@ -31,11 +31,9 @@ actor_config::actor_config(execution_unit* ptr)
 }
 
 std::string to_string(const actor_config& x) {
+  // Note: x.groups is an input range. Traversing it is emptying it, hence we
+  // cannot look inside the range here.
   std::string result = "actor_config(";
-  if (x.groups == nullptr)
-    result += "[]";
-  else
-    result += deep_to_string(*x.groups);
   auto add = [&](int flag, const char* name) {
     if ((x.flags & flag) != 0) {
       result += ", ";
