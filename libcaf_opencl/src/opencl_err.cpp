cb8588c35780a35600e731b2634b0fd310793dfd
Marian Triebe <m.triebe@live.de>
2017-12-29 18:37:37 +0100

    Fix build of OpenCL module under windows
    closes #644

--- libcaf_opencl/src/opencl_err.cpp
+++ libcaf_opencl/src/opencl_err.cpp
@@ -32,7 +32,7 @@ void throwcl(const char* fname, cl_int err) {
   }
 }
 
-void pfn_notify(const char* errinfo, const void*, size_t, void*) {
+void CL_CALLBACK pfn_notify(const char* errinfo, const void*, size_t, void*) {
   CAF_LOG_ERROR("\n##### Error message via pfn_notify #####\n"
                 << errinfo <<
                 "\n########################################");
