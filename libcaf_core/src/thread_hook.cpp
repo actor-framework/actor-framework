eab49583b56084eba226313cdba70d0a8311639a
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2018-04-11 11:48:29 +0200

    Fix weak-vtable warning

--- libcaf_core/src/thread_hook.cpp
+++ libcaf_core/src/thread_hook.cpp
@@ -16,30 +16,13 @@
  * http://www.boost.org/LICENSE_1_0.txt.                                      *
  ******************************************************************************/
 
-#pragma once
-
-#include "caf/fwd.hpp"
+#include "caf/thread_hook.hpp"
 
 namespace caf {
 
-/// Interface to define thread hooks.
-class thread_hook {
-public:
-  virtual ~thread_hook();
-
-  /// Called by the actor system once before starting any threads.
-  virtual void init(actor_system&) = 0;
-
-  /// Called whenever the actor system has started a new thread.
-  /// To access a reference to the started thread use `std::this_thread`.
-  /// @warning must the thread-safe
-  virtual void thread_started() = 0;
-
-  /// Called whenever a thread is about to quit.
-  /// To access a reference to the terminating thread use `std::this_thread`.
-  /// @warning must the thread-safe
-  virtual void thread_terminates() = 0;
-};
+thread_hook::~thread_hook() {
+  // nop
+}
 
 } // namespace caf
 
