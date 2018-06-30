fec1f77cc01842902e8dceec8141429c43c38681
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2018-06-30 19:17:21 +0200

    Add IPv4 subnet abstraction

---
+++ libcaf_core/caf/ipv4_subnet.hpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright 2011-2018 Dominik Charousset                                     *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#pragma once
+
+#include <cstdint>
+
+#include "caf/detail/comparable.hpp"
+#include "caf/ipv4_address.hpp"
+
+namespace caf {
+
+class ipv4_subnet : detail::comparable<ipv4_subnet> {
+public:
+  // -- constructors, destructors, and assignment operators --------------------
+
+  ipv4_subnet();
+
+  ipv4_subnet(ipv4_address network_address, uint8_t prefix_length);
+
+  // -- properties -------------------------------------------------------------
+
+  /// Returns the network address for this subnet.
+  inline ipv4_address network_address() const noexcept {
+    return address_;
+  }
+
+  /// Returns the prefix length of the netmask.
+  inline uint8_t prefix_length() const noexcept {
+    return prefix_length_;
+  }
+
+  /// Returns whether `addr` belongs to this subnet.
+  bool contains(ipv4_address addr) const noexcept;
+
+  /// Returns whether this subnet includes `other`.
+  bool contains(ipv4_subnet other) const noexcept;
+
+  // -- comparison -------------------------------------------------------------
+
+  int compare(const ipv4_subnet& other) const noexcept;
+
+private:
+  // -- member variables -------------------------------------------------------
+
+  ipv4_address address_;
+  uint8_t prefix_length_;
+};
+
+} // namespace caf
---
+++ libcaf_core/src/ipv4_subnet.cpp
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright 2011-2018 Dominik Charousset                                     *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#include "caf/ipv4_subnet.hpp"
+
+namespace caf {
+
+// -- constructors, destructors, and assignment operators --------------------
+
+ipv4_subnet::ipv4_subnet() {
+  // nop
+}
+
+ipv4_subnet::ipv4_subnet(ipv4_address network_address, uint8_t prefix_length)
+    : address_(network_address),
+      prefix_length_(prefix_length) {
+  // nop
+}
+
+// -- properties ---------------------------------------------------------------
+
+bool ipv4_subnet::contains(ipv4_address addr) const noexcept {
+  return address_ == addr.network_address(prefix_length_);
+}
+
+bool ipv4_subnet::contains(ipv4_subnet other) const noexcept {
+  // We can only contain a subnet if it's prefix is greater or equal.
+  if (prefix_length_ > other.prefix_length_)
+    return false;
+  return prefix_length_ == other.prefix_length_
+         ? address_ == other.address_
+         : address_ == other.address_.network_address(prefix_length_);
+}
+
+// -- comparison ---------------------------------------------------------------
+
+int ipv4_subnet::compare(const ipv4_subnet& other) const noexcept {
+  auto sub_res = address_.compare(other.address_);
+  return sub_res != 0 ? sub_res
+                      : static_cast<int>(prefix_length_) - other.prefix_length_;
+}
+
+} // namespace caf
