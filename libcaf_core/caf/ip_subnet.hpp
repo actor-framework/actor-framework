8985e1e5f3854acba7f997332e90edb012eef218
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2018-07-06 17:23:37 +0200

    Add convenience alias for IP subnets

---
+++ libcaf_core/caf/ip_subnet.hpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright 2011-2018 Dominik Charousset                                     *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#pragma once
+
+#include "caf/ipv6_subnet.hpp"
+
+namespace caf {
+
+/// An IP subnetwork. The address family is IPv6 unless `embeds_v4` returns
+/// true.
+using ip_subnet = ipv6_subnet;
+
+} // namespace caf
