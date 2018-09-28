5e0471bd1bac187e299d521ed5e4a6a03767fa8d
uentity <unientity@gmail.com>
2018-09-28 23:24:52 +0500

    Mark hardcoded defaults as explicitly visible

--- libcaf_core/caf/defaults.hpp
+++ libcaf_core/caf/defaults.hpp
@@ -23,6 +23,7 @@
 #include <string>
 #include <vector>
 
+#include "caf/config.hpp"
 #include "caf/atom.hpp"
 #include "caf/string_view.hpp"
 #include "caf/timestamp.hpp"
@@ -34,55 +35,55 @@ namespace defaults {
 
 namespace stream {
 
-extern const timespan desired_batch_complexity;
-extern const timespan max_batch_delay;
-extern const timespan credit_round_interval;
+extern CAF_API const timespan desired_batch_complexity;
+extern CAF_API const timespan max_batch_delay;
+extern CAF_API const timespan credit_round_interval;
 
 } // namespace streaming
 
 namespace scheduler {
 
-extern const atom_value policy;
-extern string_view profiling_output_file;
-extern const size_t max_threads;
-extern const size_t max_throughput;
-extern const timespan profiling_resolution;
+extern CAF_API const atom_value policy;
+extern CAF_API string_view profiling_output_file;
+extern CAF_API const size_t max_threads;
+extern CAF_API const size_t max_throughput;
+extern CAF_API const timespan profiling_resolution;
 
 } // namespace scheduler
 
 namespace work_stealing {
 
-extern const size_t aggressive_poll_attempts;
-extern const size_t aggressive_steal_interval;
-extern const size_t moderate_poll_attempts;
-extern const size_t moderate_steal_interval;
-extern const timespan moderate_sleep_duration;
-extern const size_t relaxed_steal_interval;
-extern const timespan relaxed_sleep_duration;
+extern CAF_API const size_t aggressive_poll_attempts;
+extern CAF_API const size_t aggressive_steal_interval;
+extern CAF_API const size_t moderate_poll_attempts;
+extern CAF_API const size_t moderate_steal_interval;
+extern CAF_API const timespan moderate_sleep_duration;
+extern CAF_API const size_t relaxed_steal_interval;
+extern CAF_API const timespan relaxed_sleep_duration;
 
 } // namespace work_stealing
 
 namespace logger {
 
-extern string_view component_filter;
-extern const atom_value console;
-extern string_view console_format;
-extern const atom_value console_verbosity;
-extern string_view file_format;
-extern string_view file_name;
-extern const atom_value file_verbosity;
+extern CAF_API string_view component_filter;
+extern CAF_API const atom_value console;
+extern CAF_API string_view console_format;
+extern CAF_API const atom_value console_verbosity;
+extern CAF_API string_view file_format;
+extern CAF_API string_view file_name;
+extern CAF_API const atom_value file_verbosity;
 
 } // namespace logger
 
 namespace middleman {
 
-extern std::vector<std::string> app_identifiers;
-extern const atom_value network_backend;
-extern const size_t max_consecutive_reads;
-extern const size_t heartbeat_interval;
-extern const size_t cached_udp_buffers;
-extern const size_t max_pending_msgs;
-extern const size_t workers;
+extern CAF_API std::vector<std::string> app_identifiers;
+extern CAF_API const atom_value network_backend;
+extern CAF_API const size_t max_consecutive_reads;
+extern CAF_API const size_t heartbeat_interval;
+extern CAF_API const size_t cached_udp_buffers;
+extern CAF_API const size_t max_pending_msgs;
+extern CAF_API const size_t workers;
 
 } // namespace middleman
 
