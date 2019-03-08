123506b1ad047e005530638b2628dcd8605839fa
Dominik Charousset <dominik@charousset.de>
2019-03-08 08:56:10 +0100

    Fix computation of deserialization worker count

--- libcaf_core/src/defaults.cpp
+++ libcaf_core/src/defaults.cpp
@@ -24,6 +24,7 @@
 #include <thread>
 
 using std::max;
+using std::min;
 
 namespace {
 
@@ -94,7 +95,7 @@ const size_t max_consecutive_reads = 50;
 const size_t heartbeat_interval = 0;
 const size_t cached_udp_buffers = 10;
 const size_t max_pending_msgs = 10;
-const size_t workers = max(3u, std::thread::hardware_concurrency() / 4u) + 1;
+const size_t workers = min(3u, std::thread::hardware_concurrency() / 4u) + 1;
 
 } // namespace middleman
 
