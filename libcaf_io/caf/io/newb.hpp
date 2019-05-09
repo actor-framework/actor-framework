3e34aee237034a47890d9cb0963269aaf6a84916
Jakob Otto <jakob.otto@haw-hamburg.de>
2019-05-09 13:24:22 +0200

    Fix graceful_shutdown

--- libcaf_io/caf/io/newb.hpp
+++ libcaf_io/caf/io/newb.hpp
@@ -156,8 +156,10 @@ struct newb : public network::newb_base {
       case network::operation::propagate_error: ; // nop
     }
     // Event handler reference no longer necessary.
-    if (!reading_ && !writing_)
+    if (!reading_ && !writing_) {
+      trans->shutdown(this, fd_);
       intrusive_ptr_release(this->ctrl());
+    }
   }
 
   void graceful_shutdown() override {
@@ -166,7 +168,7 @@ struct newb : public network::newb_base {
     if (state_.shutting_down)
       return;
     state_.shutting_down = true;
-    trans->shutdown(this, fd_);
+    stop_reading();
   }
 
   // -- base requirements ------------------------------------------------------
@@ -183,8 +185,6 @@ struct newb : public network::newb_base {
   void stop() override {
     CAF_PUSH_AID_FROM_PTR(this);
     CAF_LOG_TRACE("");
-    stop_reading();
-    stop_writing();
     graceful_shutdown();
   }
 
@@ -484,8 +484,10 @@ struct newb_acceptor : network::acceptor_base {
       case network::operation::propagate_error: ; // nop
     }
     // Quit if there is nothing left to do.
-    if (!reading_ && !writing_)
+    if (!reading_ && !writing_) {
+      accept_pol->shutdown(this, fd_);
       intrusive_ptr_release(this->ctrl());
+    }
   }
 
   void graceful_shutdown() override {
@@ -494,7 +496,7 @@ struct newb_acceptor : network::acceptor_base {
     if (state_.shutting_down)
       return;
     state_.shutting_down = true;
-    accept_pol->shutdown(this, fd_);
+    stop_reading();
   }
 
   // -- base requirements ------------------------------------------------------
@@ -514,8 +516,6 @@ struct newb_acceptor : network::acceptor_base {
   void stop() override {
     CAF_PUSH_AID_FROM_PTR(this);
     CAF_LOG_TRACE("");
-    stop_reading();
-    stop_writing();
     graceful_shutdown();
   }
 
