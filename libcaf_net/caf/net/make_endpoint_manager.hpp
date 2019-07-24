d2af6398608182b6ba7804efb874ff81e9d2a4e8
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2019-07-24 14:15:36 +0200

    Implement endpoint managers

---
+++ libcaf_net/caf/net/make_endpoint_manager.hpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright 2011-2019 Dominik Charousset                                     *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#pragma once
+
+#include "caf/make_counted.hpp"
+#include "caf/net/endpoint_manager.hpp"
+#include "caf/net/endpoint_manager_impl.hpp"
+
+namespace caf {
+namespace net {
+
+template <class Transport, class Application>
+endpoint_manager_ptr make_endpoint_manager(const multiplexer_ptr& mpx,
+                                           actor_system& sys, Transport trans,
+                                           Application app) {
+  using impl = endpoint_manager_impl<Transport, Application>;
+  return make_counted<impl>(mpx, sys, std::move(trans), std::move(app));
+}
+
+} // namespace net
+} // namespace caf
