a364e80abc457d94dc102e0998fdfccb2489cfe4
Dominik Charousset <dominik@charousset.de>
2019-08-31 01:35:58 +0200

    Drop complex Git logic from CMake
    Fix get-release-version.sh and rely on the CI calling the script before
    invoking CMake when building documentation.

--- scripts/get-release-version.sh
+++ scripts/get-release-version.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-grep "define CAF_VERSION" libcaf_core/caf/config.hpp | awk '{printf "%d.%d.%d", int($3 / 10000), int($3 / 100) % 100, $3 % 100}' > version.txt
-git log --pretty=format:%h -n 1 > sha.txt
-if test "$(cat branch.txt) = master" && git describe --tags --contains $(cat sha.txt) 1>tag.txt 2>/dev/null ; then
-  cp tag.txt release.txt
-else
-  printf "%s" "$(cat version.txt)+exp.sha.$(cat sha.txt)" > release.txt
+# Get string representation of CAF version.
+caf_version=`grep "define CAF_VERSION" libcaf_core/caf/config.hpp | awk '{ printf "%d.%d.%d", int($3 / 10000), int($3 / 100) % 100, $3 % 100 }'`
+
+# Get SHA from Git.
+git_sha=`git log --pretty=format:%h -n 1`
+
+# Check whether the current SHA is a tag.
+if git describe --tags --contains $git_sha 1>release.txt 2>/dev/null
+then
+  # Tags indicate stable release -> use tag version.
+  # On success, we'll have the tag version in release.txt now, so we're done.
+  exit 0
 fi
+
+# Generate default release version.
+echo "$caf_version+exp.sha.$git_sha" >release.txt
