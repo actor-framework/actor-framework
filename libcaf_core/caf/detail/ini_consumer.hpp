b46d7aadedc9d96150187637ee00e3874a525406
Dominik Charousset <dominik.charousset@corelight.com>
2019-09-12 14:17:12 +0200

    Fix defaulted-function-deleted warning

--- libcaf_core/caf/detail/ini_consumer.hpp
+++ libcaf_core/caf/detail/ini_consumer.hpp
@@ -193,8 +193,6 @@ public:
 
   ini_consumer(config_option_set& options, config_map& cfg);
 
-  ini_consumer(ini_consumer&&) = default;
-
   // -- properties -------------------------------------------------------------
 
   ini_category_consumer begin_map();
