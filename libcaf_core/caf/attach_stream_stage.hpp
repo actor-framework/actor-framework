fd14095dfdfbc6b88379e2b2c896292aac50f8cf
Dominik Charousset <dominik.charousset@corelight.com>
2019-09-28 14:10:56 +0200

    Fix static assertion

--- libcaf_core/caf/attach_stream_stage.hpp
+++ libcaf_core/caf/attach_stream_stage.hpp
@@ -18,6 +18,8 @@
 
 #pragma once
 
+#include <vector>
+
 #include "caf/default_downstream_manager.hpp"
 #include "caf/detail/stream_stage_driver_impl.hpp"
 #include "caf/detail/stream_stage_impl.hpp"
@@ -74,10 +76,14 @@ attach_stream_stage(scheduled_actor* self, const stream<In>& in,
                   void(state_type&),
                   typename detail::get_callable_trait<Init>::fun_sig>::value,
                 "Expected signature `void (State&)` for init function");
-  static_assert(std::is_same<
-                  void(state_type&, downstream<output_type>&, In),
-                  typename detail::get_callable_trait<Fun>::fun_sig>::value,
+  using consume_one = void(state_type&, downstream<output_type>&, In);
+  using consume_all = void(state_type&, downstream<output_type>&,
+                           std::vector<In>&);
+  using fun_sig = typename detail::get_callable_trait<Fun>::fun_sig;
+  static_assert(std::is_same<fun_sig, consume_one>::value
+                  || std::is_same<fun_sig, consume_all>::value,
                 "Expected signature `void (State&, downstream<Out>&, In)` "
+                "or `void (State&, downstream<Out>&, std::vector<In>&)` "
                 "for consume function");
   using driver = detail::stream_stage_driver_impl<
     typename Trait::input, DownstreamManager, Fun, Finalize>;
