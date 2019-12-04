8b97c94c2dab127af3b33233ff7f349e653b2199
Luca Rinaldi <lucarin@protonmail.com>
2019-12-04 13:08:13 +0100

    update affinity test

--- examples/affinity_test.cpp
+++ examples/affinity_test.cpp
@@ -4,33 +4,42 @@
 
 #ifdef CAF_LINUX
 #  include <sys/sysinfo.h>
-#endif // CAF_LINUX
+#endif
 
 using std::endl;
 using std::string;
 using std::to_string;
+using std::vector;
 
 using namespace caf;
 
 using print_core = atom_constant<atom("print_core")>;
 
-// get the affinity configuration of the current thread
+// Get the affinity configuration of the current thread
 string get_my_affinity() {
   string res = "  cores( ";
 #if defined(CAF_LINUX)
   cpu_set_t mask;
-  if (sched_getaffinity(0, sizeof(cpu_set_t), &mask) == -1) {
-    perror("sched_getaffinity");
-    return "";
-  }
-  auto nproc = get_nprocs();
-  for (auto i = 0; i < nproc; i++) {
-    if (CPU_ISSET(i, &mask)) {
-      res += std::to_string(i) + " ";
+  if (sched_getaffinity(0, sizeof(cpu_set_t), &mask) == 0) {
+    auto nproc = get_nprocs();
+    for (auto i = 0; i < nproc; i++) {
+      if (CPU_ISSET(i, &mask)) {
+        res += to_string(i) + " ";
+      }
     }
   }
 #elif defined(CAF_WINDOWS)
-  // TODO: implement getaffinity for windows
+// TODO: implement getaffinity for Windows
+#elif defined(CAF_MACOS)
+  thread_affinity_policy affinity;
+  if (thread_policy_get(mach_thread_self(), THREAD_AFFINITY_POLICY,
+                        (thread_policy_t) &affinity,
+                        THREAD_AFFINITY_POLICY_COUNT, FALSE)
+      == 0) {
+    res += to_string(affinity.affinity_tag) + " ";
+  }
+#elif defined(CAF_BSD)
+  // TODO: implement getaffinity for FreeBSD
 #endif
   res += ")";
   return res;
@@ -54,7 +63,31 @@ void block_actor(blocking_actor* self) {
   });
 }
 
-void caf_main(actor_system& system) {
+int main() {
+// Set affinity configuration
+#ifndef CAF_MACOS
+  // Windows, Linux and FreeBSD start counting cores from 0.
+  actor_system_config cfg;
+  // Pin the scheduled actors on core 0 and 1.
+  cfg.set<vector<vector<int>>>("affinity.scheduled-actors", {{0, 1}});
+  // Pin the first detached actor on core 0 and the second on core 1.
+  cfg.set<vector<vector<int>>>("affinity.detached-actors", {{0}, {1}});
+  // Pin the blocking actors on core 1.
+  cfg.set<vector<vector<int>>>("affinity.blocking-actors", {{1}});
+#else
+  // Mac start counting cores from 1 and cannot set multiple cores per thread.
+  actor_system_config cfg;
+  // Pin the scheduled actors on core 2.
+  cfg.set<vector<vector<int>>>("affinity.scheduled-actors", {{2}});
+  // Pin the first detached actor on core 1 and the second on core 2.
+  cfg.set<vector<vector<int>>>("affinity.detached-actors", {{1}, {2}});
+  // Pin the blocking actors on core 1.
+  cfg.set<vector<vector<int>>>("affinity.blocking-actors", {{1}});
+#endif
+
+  // Spawn all kind of actors and print the affinity configuration
+  // of their undelining thread.
+  actor_system system{cfg};
   anon_send(system.spawn(event_actor), print_core::value);
   anon_send(system.spawn(event_actor), print_core::value);
   anon_send(system.spawn<detached>(det_actor), print_core::value);
@@ -62,5 +95,3 @@ void caf_main(actor_system& system) {
   anon_send(system.spawn(block_actor), print_core::value);
   anon_send(system.spawn(block_actor), print_core::value);
 }
-
-CAF_MAIN()
