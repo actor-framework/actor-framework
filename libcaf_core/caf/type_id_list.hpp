9a5dbc12529c53c58866f8762ccb8f693918f69d
Dominik Charousset <dominik@charousset.de>
2020-01-23 17:47:31 +0100

    Add new type ID list

---
+++ libcaf_core/caf/type_id_list.hpp
@@ -0,0 +1,93 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright 2011-2020 Dominik Charousset                                     *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <string>
+
+#include "caf/detail/comparable.hpp"
+#include "caf/detail/core_export.hpp"
+#include "caf/type_id.hpp"
+
+namespace caf {
+
+/// A list of 16-bit type IDs, stored in a size-prefix, contiguous memory block.
+class type_id_list : detail::comparable<type_id_list> {
+public:
+  using pointer = const uint16_t*;
+
+  constexpr explicit type_id_list(pointer data) noexcept : data_(data) {
+    // nop
+  }
+
+  constexpr type_id_list(const type_id_list&) noexcept = default;
+
+  type_id_list& operator=(const type_id_list&) noexcept = default;
+
+  /// Queries whether this type list contains data, i,e, `data() != nullptr`.
+  constexpr operator bool() const noexcept {
+    return data_ != nullptr;
+  }
+
+  /// Returns the raw pointer to the size-prefixed list.
+  constexpr pointer data() const noexcept {
+    return data_;
+  }
+
+  /// Returns the number of elements in the list.
+  /// @pre `data() != nullptr`
+  constexpr size_t size() const noexcept {
+    return data_[0];
+  }
+
+  /// Returns the type ID at `index`.
+  /// @pre `data() != nullptr`
+  constexpr uint16_t operator[](size_t index) const noexcept {
+    return data_[index + 1];
+  }
+
+  /// Compares this list to `other`.
+  int compare(type_id_list other) const noexcept {
+    return memcmp(data_, other.data_, size() * sizeof(uint16_t));
+  }
+
+private:
+  pointer data_;
+};
+
+/// @private
+template <class... Ts>
+struct make_type_id_list_helper {
+  static constexpr inline uint16_t data[]
+    = {static_cast<uint16_t>(sizeof...(Ts)), type_id_v<Ts>...};
+};
+
+/// Constructs a ::type_id_list from the template parameter pack `Ts`.
+/// @relates type_id_list
+template <class... Ts>
+constexpr type_id_list make_type_id_list() {
+  return type_id_list{make_type_id_list_helper<Ts...>::data};
+}
+
+/// @relates type_id_list
+CAF_CORE_EXPORT std::string to_string(type_id_list xs);
+
+} // namespace caf
---
+++ libcaf_core/test/type_id_list.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright 2011-2020 Dominik Charousset                                     *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#define CAF_SUITE type_id_list
+
+#include "caf/type_id_list.hpp"
+
+#include "caf/test/dsl.hpp"
+
+#include "caf/init_global_meta_objects.hpp"
+
+using namespace caf;
+
+CAF_TEST(lists store the size at index 0) {
+  uint16_t data[] = {3, 1, 2, 4};
+  type_id_list xs{data};
+  CAF_CHECK_EQUAL(xs.size(), 3u);
+  CAF_CHECK_EQUAL(xs[0], 1u);
+  CAF_CHECK_EQUAL(xs[1], 2u);
+  CAF_CHECK_EQUAL(xs[2], 4u);
+}
+
+CAF_TEST(lists are comparable) {
+  uint16_t data[] = {3, 1, 2, 4};
+  type_id_list xs{data};
+  uint16_t data_copy[] = {3, 1, 2, 4};
+  type_id_list ys{data_copy};
+  CAF_CHECK_EQUAL(xs, ys);
+  data_copy[1] = 10;
+  CAF_CHECK_NOT_EQUAL(xs, ys);
+  CAF_CHECK_LESS(xs, ys);
+}
+
+CAF_TEST(make_type_id_list constructs a list from types) {
+  auto xs = make_type_id_list<uint8_t, bool, float>();
+  CAF_CHECK_EQUAL(xs.size(), 3u);
+  CAF_CHECK_EQUAL(xs[0], type_id_v<uint8_t>);
+  CAF_CHECK_EQUAL(xs[1], type_id_v<bool>);
+  CAF_CHECK_EQUAL(xs[2], type_id_v<float>);
+}
+
+CAF_TEST(type ID lists are convertible to strings) {
+  detail::clear_global_meta_objects();
+  init_global_meta_objects<builtin_type_ids>();
+  auto xs = make_type_id_list<uint8_t, bool, float>();
+  CAF_CHECK_EQUAL(to_string(xs), "[uint8_t, bool, float]");
+}
