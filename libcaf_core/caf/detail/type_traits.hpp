9c4a94ef139cb966b28657a372474266063df87c
Dominik Charousset <dominik@charousset.de>
2020-05-04 15:14:48 +0200

    Clear target variables before parsing
    (cherry picked from commit ba142d6885a120fe4bc68c09a72455f4f8188101)

--- libcaf_core/caf/detail/type_traits.hpp
+++ libcaf_core/caf/detail/type_traits.hpp
@@ -455,9 +455,10 @@ public:
   static constexpr bool value = sizeof(fun(static_cast<T*>(nullptr))) > 1;
 };
 
-CAF_HAS_MEMBER_TRAIT(size);
+CAF_HAS_MEMBER_TRAIT(clear);
 CAF_HAS_MEMBER_TRAIT(data);
 CAF_HAS_MEMBER_TRAIT(make_behavior);
+CAF_HAS_MEMBER_TRAIT(size);
 
 /// Checks whether F is convertible to either `std::function<void (T&)>`
 /// or `std::function<void (const T&)>`.
