0db5680eeb5583d393ec1197e882a008c0741b26
Dominik Charousset <dominik@charousset.de>
2020-06-16 11:48:40 +0200

    Combine gauge unit tests into a single file

--- libcaf_core/test/telemetry/gauge.cpp
+++ libcaf_core/test/telemetry/gauge.cpp
@@ -16,13 +16,32 @@
  * http://www.boost.org/LICENSE_1_0.txt.                                      *
  ******************************************************************************/
 
-#define CAF_SUITE telemetry.int_gauge
+#define CAF_SUITE telemetry.gauge
 
-#include "caf/telemetry/int_gauge.hpp"
+#include "caf/telemetry/gauge.hpp"
 
 #include "caf/test/dsl.hpp"
 
 using namespace caf;
+
+CAF_TEST(double gauges can increment and decrement) {
+  telemetry::dbl_gauge g;
+  CAF_MESSAGE("gauges start at 0");
+  CAF_CHECK_EQUAL(g.value(), 0.0);
+  CAF_MESSAGE("gauges are incrementable");
+  g.inc();
+  g.inc(2.0);
+  CAF_CHECK_EQUAL(g.value(), 3.0);
+  CAF_MESSAGE("gauges are decrementable");
+  g.dec();
+  g.dec(5.0);
+  CAF_CHECK_EQUAL(g.value(), -3.0);
+  CAF_MESSAGE("gauges allow setting values");
+  g.value(42.0);
+  CAF_CHECK_EQUAL(g.value(), 42.0);
+  CAF_MESSAGE("users can create gauges with custom start values");
+  CAF_CHECK_EQUAL(telemetry::dbl_gauge{42.0}.value(), 42.0);
+}
 
 CAF_TEST(integer gauges can increment and decrement) {
   telemetry::int_gauge g;
