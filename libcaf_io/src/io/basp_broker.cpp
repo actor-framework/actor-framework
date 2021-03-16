dc26020feabb3a274618548c051b71e17cafa875
Dominik Charousset <dominik@charousset.de>
2021-03-16 16:47:16 +0100

    Add connection_timeout to debug log output

--- libcaf_io/src/io/basp_broker.cpp
+++ libcaf_io/src/io/basp_broker.cpp
@@ -109,12 +109,13 @@ behavior basp_broker::make_behavior() {
   auto heartbeat_interval = get_or(config(), "caf.middleman.heartbeat-interval",
                                    defaults::middleman::heartbeat_interval);
   if (heartbeat_interval.count() > 0) {
-    CAF_LOG_DEBUG("enable heartbeat" << CAF_ARG(heartbeat_interval));
     auto now = clock().now();
     auto first_tick = now + heartbeat_interval;
     auto connection_timeout = get_or(config(),
                                      "caf.middleman.connection-timeout",
                                      defaults::middleman::connection_timeout);
+    CAF_LOG_DEBUG("enable heartbeat" << CAF_ARG(heartbeat_interval)
+                                     << CAF_ARG(connection_timeout));
     // Note: we send the scheduled time as integer representation to avoid
     //       having to assign a type ID to the time_point type.
     scheduled_send(this, first_tick, tick_atom_v,
