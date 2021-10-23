c18a1e1af59fe351ca2c351b762903e41d2c4c49
Dominik Charousset <dominik@charousset.de>
2021-10-23 15:11:09 +0200

    Use compile-time constants as defaults

--- libcaf_net/caf/net/defaults.hpp
+++ libcaf_net/caf/net/defaults.hpp
@@ -7,24 +7,17 @@
 #include <cstddef>
 #include <cstdint>
 
-#include "caf/detail/net_export.hpp"
-
 // -- hard-coded default values for various CAF options ------------------------
 
 namespace caf::defaults::middleman {
 
 /// Maximum number of cached buffers for sending payloads.
-CAF_NET_EXPORT extern const size_t max_payload_buffers;
+constexpr size_t max_payload_buffers = 100;
 
 /// Maximum number of cached buffers for sending headers.
-CAF_NET_EXPORT extern const size_t max_header_buffers;
+constexpr size_t max_header_buffers = 10;
 
 /// Port to listen on for tcp.
-CAF_NET_EXPORT extern const uint16_t tcp_port;
-
-/// Caps how much Bytes a stream transport pushes to its write buffer before
-/// stopping to read from its message queue. Default TCP send buffer is 16kB (at
-/// least on Linux).
-constexpr auto stream_output_buf_cap = size_t{32768};
+constexpr uint16_t tcp_port = 0;
 
 } // namespace caf::defaults::middleman
