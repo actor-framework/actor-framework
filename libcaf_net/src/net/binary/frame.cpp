6ada45beac6800c40124ee0a724c309b545af5d8
Dominik Charousset <dominik@charousset.de>
2022-10-29 08:19:54 +0200

    Allow merging multiple buffers into caf.net frames

--- libcaf_net/src/net/binary/frame.cpp
+++ libcaf_net/src/net/binary/frame.cpp
@@ -6,14 +6,34 @@
 
 #include <cstring>
 #include <new>
+#include <numeric>
 
 namespace caf::net::binary {
 
-frame::frame(const_byte_span data) {
-  auto total_size = sizeof(web_socket::frame::data) + data.size();
-  auto vptr = malloc(total_size);
-  data_.reset(new (vptr) web_socket::frame::data(true, data.size()), false);
-  memcpy(data_->storage(), data.data(), data.size());
+namespace {
+
+template <class Buffer>
+size_t calc_total_size(span<const Buffer> buffers) {
+  auto fn = [](size_t n, const Buffer& buf) { return n + buf.size(); };
+  return std::accumulate(buffers.begin(), buffers.end(), size_t{0}, fn);
+}
+
+} // namespace
+
+frame::frame(const_byte_span buf) {
+  init(buf.size(), buf);
+}
+
+frame::frame(caf::span<const const_byte_span> bufs) {
+  auto payload_size = calc_total_size(bufs);
+  init(payload_size, payload_size, bufs);
+}
+
+template <class... Args>
+void frame::init(size_t payload_size, Args&&... args) {
+  using data_t = web_socket::frame::data;
+  auto vptr = malloc(sizeof(data_t) + payload_size);
+  data_.reset(new (vptr) data_t(std::forward<Args>(args)...), false);
 }
 
 } // namespace caf::net::binary
