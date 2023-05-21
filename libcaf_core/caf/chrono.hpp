e576f54ea277fbe791939e51ff730bdd11995261
Dominik Charousset <dominik@charousset.de>
2023-05-21 17:27:02 +0200

    Implement better utilities for timestamp parsing

---
+++ libcaf_core/caf/chrono.hpp
@@ -0,0 +1,159 @@
+// This file is part of CAF, the C++ Actor Framework. See the file LICENSE in
+// the main distribution directory for license terms and copyright or visit
+// https://github.com/actor-framework/actor-framework/blob/master/LICENSE.
+
+#pragma once
+
+#include "caf/detail/core_export.hpp"
+#include "caf/fwd.hpp"
+#include "caf/parser_state.hpp"
+
+#include <chrono>
+#include <ctime>
+#include <optional>
+#include <string>
+#include <string_view>
+
+namespace caf::detail {
+
+/// The size of the buffer used for formatting.
+constexpr size_t format_buffer_size = 40;
+
+/// Splits the time point into seconds and nanoseconds since epoch.
+template <class TimePoint>
+std::pair<time_t, int> split_time_point(TimePoint ts) {
+  namespace sc = std::chrono;
+  using clock_type = sc::system_clock;
+  using clock_timestamp = typename clock_type::time_point;
+  using clock_duration = typename clock_type::duration;
+  auto tse = ts.time_since_epoch();
+  clock_timestamp as_sys_time{sc::duration_cast<clock_duration>(tse)};
+  auto secs = clock_type::to_time_t(as_sys_time);
+  auto ns = sc::duration_cast<sc::nanoseconds>(tse).count() % 1'000'000'000;
+  return {secs, static_cast<int>(ns)};
+}
+
+/// Prints the date and time to a buffer.
+/// @param buf The buffer to write to.
+/// @param buf_size The size of `buf` in bytes.
+/// @param secs The number of seconds since epoch.
+/// @param nsecs The number of nanoseconds since the last full second.
+/// @returns A pointer to the null terminator of `buf`.
+/// @pre `buf_size >= 36`.
+CAF_CORE_EXPORT
+char* print_localtime(char* buf, size_t buf_size, time_t secs,
+                      int nsecs) noexcept;
+
+} // namespace caf::detail
+
+namespace caf::chrono {
+
+// TODO: becomes obsolete when switching to C++20.
+template <class Duration>
+using sys_time = std::chrono::time_point<std::chrono::system_clock, Duration>;
+
+/// Formats `ts` in ISO 8601 format.
+/// @param ts The time point to format.
+/// @returns The formatted string.
+template <class Duration>
+std::string to_string(sys_time<Duration> ts) {
+  char buf[detail::format_buffer_size];
+  auto [secs, nsecs] = detail::split_time_point(ts);
+  detail::print_localtime(buf, sizeof(buf), secs, nsecs);
+  return buf;
+}
+
+/// Prints `ts` to `out` in ISO 8601 format.
+/// @param out The buffer to write to.
+/// @param ts The time point to print.
+template <class Buffer, class Duration>
+void print(Buffer& out, sys_time<Duration> ts) {
+  char buf[detail::format_buffer_size];
+  auto [secs, nsecs] = detail::split_time_point(ts);
+  auto end = detail::print_localtime(buf, sizeof(buf), secs, nsecs);
+  out.insert(out.end(), buf, end);
+}
+
+/// Represents a point in time, expressed as a date and time of day. Also
+/// provides formatting and parsing functionality for ISO 8601.
+class CAF_CORE_EXPORT datetime {
+public:
+  /// The year.
+  int year = 0;
+
+  /// The month of the year, starting with 1 for January.
+  int month = 0;
+
+  /// The day of the month, starting with 1.
+  int day = 0;
+
+  /// The hour of the day, starting with 0.
+  int hour = 0;
+
+  /// The minute of the hour, starting with 0.
+  int minute = 0;
+
+  /// The second of the minute, starting with 0.
+  int second = 0;
+
+  /// The nanosecond of the second, starting with 0.
+  int nanosecond = 0;
+
+  /// The offset from UTC in seconds.
+  std::optional<int> utc_offset;
+
+  /// Returns whether this object contains a valid date and time. A default
+  /// constructed object is invalid.
+  bool valid() const noexcept;
+
+  /// Returns whether this object is equal to `other`.
+  bool equals(const datetime& other) const noexcept;
+
+  /// Parses a date and time string in ISO 8601 format.
+  /// @param str The string to parse.
+  /// @returns The parsed date and time.
+  error parse(std::string_view str);
+
+  /// Convenience function for converting a string to a `datetime` object.
+  static expected<datetime> from_string(std::string_view str);
+
+  /// Parses a date and time string in ISO 8601 format.
+  /// @param ps The parser state to use.
+  void parse(string_parser_state& ps);
+
+  /// Overrides the current date and time with the values from `x`.
+  void value(const datetime& x) noexcept {
+    *this = x;
+  }
+
+  /// Converts this object to a time_point object with given Duration type.
+  template <class Duration = std::chrono::system_clock::duration>
+  auto to_local_time() const noexcept {
+    // TODO: consider using year_month_day once we have C++20.
+    namespace sc = std::chrono;
+    auto converted = sc::system_clock::from_time_t(to_time_t());
+    auto result = sc::time_point_cast<Duration>(converted);
+    auto ns = sc::nanoseconds{nanosecond};
+    result += sc::duration_cast<Duration>(ns);
+    return sc::time_point_cast<Duration>(result);
+  }
+
+private:
+  /// Converts this object to a time_t value.
+  time_t to_time_t() const noexcept;
+};
+
+/// @relates datetime
+inline bool operator==(const datetime& x, const datetime& y) noexcept {
+  return x.equals(y);
+}
+
+/// @relates datetime
+inline bool operator!=(const datetime& x, const datetime& y) noexcept {
+  return !(x == y);
+}
+
+/// @relates datetime
+std::string to_string(const datetime& x);
+
+} // namespace caf::chrono
---
+++ libcaf_core/caf/detail/parser/read_timestamp.hpp
@@ -0,0 +1,163 @@
+// This file is part of CAF, the C++ Actor Framework. See the file LICENSE in
+// the main distribution directory for license terms and copyright or visit
+// https://github.com/actor-framework/actor-framework/blob/master/LICENSE.
+
+#pragma once
+
+#include "caf/chrono.hpp"
+#include "caf/config.hpp"
+#include "caf/detail/consumer.hpp"
+#include "caf/detail/parser/add_ascii.hpp"
+#include "caf/detail/parser/chars.hpp"
+#include "caf/detail/parser/read_signed_integer.hpp"
+#include "caf/detail/scope_guard.hpp"
+
+CAF_PUSH_UNUSED_LABEL_WARNING
+
+#include "caf/detail/parser/fsm.hpp"
+
+namespace caf::detail::parser {
+
+// Parses an integer in the form "00". We can't use `read_int` here because
+// it will interpret the leading zero as an octal prefix.
+template <class State, class Consumer>
+void read_two_digit_int(State& ps, Consumer&& consumer) {
+  auto result = 0;
+  auto g = make_scope_guard([&] {
+    if (ps.code <= pec::trailing_character) {
+      consumer.value(result);
+    }
+  });
+  // clang-format off
+  start();
+  state(init) {
+    transition(at_digit2, decimal_chars, add_ascii<10>(result, ch))
+  }
+  state(at_digit2) {
+    transition(done, decimal_chars, add_ascii<10>(result, ch))
+  }
+  term_state(done) {
+    // nop
+  }
+  fin();
+  // clang-format on
+}
+
+/// Reads a UTC offset in ISO 8601 format, i.e., a string of the form
+/// `[+-]HH:MM`, `[+-]HHMM`, or `[+-]HH`.
+template <class State, class Consumer>
+void read_utc_offset(State& ps, Consumer&& consumer) {
+  bool negative = false;
+  auto hh = 0;
+  auto mm = 0;
+  auto g = make_scope_guard([&] {
+    if (ps.code <= pec::trailing_character) {
+      auto result = hh * 3600 + mm * 60;
+      consumer.value(negative ? -result : result);
+    }
+  });
+  // clang-format off
+  start();
+  state(init) {
+    fsm_transition(read_two_digit_int(ps, make_consumer(hh)), after_hh, '+')
+    fsm_transition(read_two_digit_int(ps, make_consumer(hh)), after_hh, '-',
+                   negative = true)
+  }
+  term_state(after_hh) {
+    fsm_transition(read_two_digit_int(ps, make_consumer(mm)), done, ':')
+    fsm_epsilon(read_two_digit_int(ps, make_consumer(mm)), done, decimal_chars)
+  }
+  term_state(done) {
+    // nop
+  }
+  fin();
+  // clang-format on
+}
+
+/// Reads a date and time in ISO 8601 format.
+template <class State, class Consumer>
+void read_timestamp(State& ps, Consumer&& consumer) {
+  auto fractional_seconds = 0;
+  auto fractional_seconds_decimals = 0;
+  auto add_fractional = [&](char ch) {
+    ++fractional_seconds_decimals;
+    return add_ascii<10>(fractional_seconds, ch);
+  };
+  chrono::datetime result;
+  auto g = make_scope_guard([&] {
+    if (ps.code <= pec::trailing_character) {
+      if (fractional_seconds_decimals > 0) {
+        auto divisor = 1;
+        for (auto i = 0; i < fractional_seconds_decimals; ++i)
+          divisor *= 10;
+        result.nanosecond = fractional_seconds * (1'000'000'000 / divisor);
+      }
+      if (result.valid())
+        consumer.value(result);
+      else
+        ps.code = pec::invalid_argument;
+    }
+  });
+  // clang-format off
+  start();
+  state(init) {
+    fsm_epsilon(read_signed_integer(ps, make_consumer(result.year)),
+                has_year, decimal_chars);
+  }
+  state(has_year) {
+    fsm_transition(read_two_digit_int(ps, make_consumer(result.month)),
+                   has_month, '-')
+  }
+  state(has_month) {
+    fsm_transition(read_two_digit_int(ps, make_consumer(result.day)),
+                   has_day, '-')
+  }
+  state(has_day) {
+    transition(at_hour, 'T')
+  }
+  state(at_hour) {
+    fsm_epsilon(read_two_digit_int(ps, make_consumer(result.hour)),
+                has_hour, decimal_chars);
+  }
+  state(has_hour) {
+    transition(at_minute, ':')
+  }
+  state(at_minute) {
+    fsm_epsilon(read_two_digit_int(ps, make_consumer(result.minute)),
+                has_minute, decimal_chars);
+  }
+  state(has_minute) {
+    transition(at_second, ':')
+  }
+  state(at_second) {
+    fsm_epsilon(read_two_digit_int(ps, make_consumer(result.second)),
+                has_second, decimal_chars);
+  }
+  term_state(has_second) {
+    transition(has_fractional, '.')
+    transition(done, 'Z', result.utc_offset = 0)
+    fsm_epsilon(read_utc_offset(ps, make_consumer(result.utc_offset.emplace(0))),
+                done, "+-")
+  }
+  term_state(has_fractional) {
+    transition_if(fractional_seconds_decimals < 9, has_fractional, decimal_chars,
+                  add_fractional(ch))
+    epsilon_if(fractional_seconds_decimals > 0, has_utc_offset)
+  }
+  term_state(has_utc_offset) {
+    transition(done, 'Z', result.utc_offset = 0)
+    fsm_epsilon(read_utc_offset(ps, make_consumer(result.utc_offset.emplace(0))),
+                done, "+-")
+  }
+  term_state(done) {
+    // nop
+  }
+  fin();
+  // clang-format on
+}
+
+} // namespace caf::detail::parser
+
+#include "caf/detail/parser/fsm_undef.hpp"
+
+CAF_POP_WARNINGS
