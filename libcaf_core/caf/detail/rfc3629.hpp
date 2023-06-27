eaa0a2283970e8772f9231df55900202b13dcecb
Dominik Charousset <dominik@charousset.de>
2023-06-27 12:13:35 +0200

    Add utility for UTF-8 validation

---
+++ libcaf_core/caf/detail/rfc3629.hpp
@@ -0,0 +1,27 @@
+// This file is part of CAF, the C++ Actor Framework. See the file LICENSE in
+// the main distribution directory for license terms and copyright or visit
+// https://github.com/actor-framework/actor-framework/blob/master/LICENSE.
+
+#pragma once
+
+#include "caf/byte_span.hpp"
+#include "caf/detail/core_export.hpp"
+
+#include <cstddef>
+
+namespace caf::detail {
+
+/// Wraps functions for processing RFC 3629 encoding, i.e., UTF-8. See
+/// https://datatracker.ietf.org/doc/html/rfc3629 for details.
+class CAF_CORE_EXPORT rfc3629 {
+public:
+  /// Checks whether `bytes` is a valid UTF-8 string.
+  static bool valid(const_byte_span bytes) noexcept;
+
+  /// Checks whether `str` is a valid UTF-8 string.
+  static bool valid(std::string_view str) noexcept {
+    return valid(as_bytes(make_span(str)));
+  }
+};
+
+} // namespace caf::detail
