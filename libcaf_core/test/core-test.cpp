59cc884efa90af24acbbb654b109406f533d9ef5
Dominik Charousset <dominik@charousset.de>
2023-07-14 10:55:13 +0200

    Use the new project structure for caf_core

--- libcaf_core/test/core-test.cpp
+++
@@ -1,7 +0,0 @@
-// This file is part of CAF, the C++ Actor Framework. See the file LICENSE in
-// the main distribution directory for license terms and copyright or visit
-// https://github.com/actor-framework/actor-framework/blob/master/LICENSE.
-
-#include "caf/test/caf_test_main.hpp"
-
-CAF_TEST_MAIN()
