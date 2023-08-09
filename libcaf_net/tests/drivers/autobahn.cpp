c2a60ac74dc6b596ae4097adc7a9ae549fa3d1ef
Dominik Charousset <dominik@charousset.de>
2023-08-09 16:54:03 +0200

    Implement missing autobahn driver scaffolding

--- libcaf_net/tests/drivers/autobahn.cpp
+++ libcaf_net/tests/drivers/autobahn.cpp
@@ -1,18 +1,21 @@
 // Simple WebSocket server that sends everything it receives back to the sender.
 
 #include "caf/net/middleman.hpp"
-#include "caf/net/octet_stream/transport.hpp"
-#include "caf/net/web_socket/acceptor.hpp"
-#include "caf/net/web_socket/framing.hpp"
-#include "caf/net/web_socket/lower_layer.hpp"
 #include "caf/net/web_socket/upper_layer.hpp"
+#include "caf/net/web_socket/with.hpp"
 
 #include "caf/actor_system.hpp"
 #include "caf/actor_system_config.hpp"
 #include "caf/caf_main.hpp"
 
+#include <cstdio>
 #include <iostream>
 #include <utility>
+
+using namespace std::literals;
+
+namespace ssl = caf::net::ssl;
+namespace ws = caf::net::web_socket;
 
 // -- constants ----------------------------------------------------------------
 
@@ -44,7 +47,7 @@ public:
     return caf::none;
   }
 
-  caf::error accept(const caf::net::http::request_header& hdr) override {
+  caf::error accept(const caf::net::http::request_header&) override {
     return caf::none;
   }
 
@@ -80,23 +83,48 @@ public:
 };
 
 int caf_main(caf::actor_system& sys, const config& cfg) {
-  namespace ws = caf::net::web_socket;
   // Read the configuration.
+  auto interval = caf::get_or(cfg, "interval", caf::timespan{1s});
   auto port = caf::get_or(cfg, "port", default_port);
+  auto pem = ssl::format::pem;
+  auto key_file = caf::get_as<std::string>(cfg, "tls.key-file");
+  auto cert_file = caf::get_as<std::string>(cfg, "tls.cert-file");
   auto max_connections = caf::get_or(cfg, "max-connections",
                                      default_max_connections);
-
-  auto app_layer = web_socket_app::make();
-  auto app = app_layer.get();
-  auto uut_layer
-    = caf::net::web_socket::framing::make_server(std::move(app_layer));
-  auto uut = uut_layer.get();
-
-  auto transport
-    = caf::net::octet_stream::transport::make(std::move(uut_layer));
-
-  // transport->configure_read(caf::net::receive_policy::up_to(2048u));
-
+  if (!key_file != !cert_file) {
+    std::cerr << "*** inconsistent TLS config: declare neither file or both\n";
+    return EXIT_FAILURE;
+  }
+  // Open up a TCP port for incoming connections and start the server.
+  using trait = ws::default_trait;
+  auto server
+    = ws::with(sys)
+        // Optionally enable TLS.
+        .context(ssl::context::enable(key_file && cert_file)
+                   .and_then(ssl::emplace_server(ssl::tls::v1_2))
+                   .and_then(ssl::use_private_key_file(key_file, pem))
+                   .and_then(ssl::use_certificate_file(cert_file, pem)))
+        // Bind to the user-defined port.
+        .accept(port)
+        // Limit how many clients may be connected at any given time.
+        .max_connections(max_connections)
+        // Add handler for incoming connections.
+        .on_request([](ws::acceptor<>& acc) {
+          // Ignore all header fields and accept the connection.
+          acc.accept();
+        })
+        // When started, run our worker actor to handle incoming connections.
+        .start([] { return web_socket_app::make(); });
+  // Report any error to the user.
+  if (!server) {
+    std::cerr << "*** unable to run at port " << port << ": "
+              << to_string(server.error()) << '\n';
+    return EXIT_FAILURE;
+  }
+  // Run until the user stops the server by pressing enter.
+  std::cout << "*** running on port " << port << '\n'
+            << "*** press [enter] to quit" << std::endl;
+  getchar();
   return EXIT_SUCCESS;
 }
 
