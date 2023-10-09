3c27a4e71cd53e9313b31f942872a7f35622c7bf
Samir Halilcevic <s.halilcevic@interance.io>
2023-10-09 15:12:00 +0200

    Fix encoding of chunk size in HTTP

--- libcaf_net/tests/legacy/net/http/server.cpp
+++ libcaf_net/tests/legacy/net/http/server.cpp
@@ -23,6 +23,8 @@ public:
 
   net::http::lower_layer* down = nullptr;
 
+  bool chunked_response = false;
+
   // -- properties -------------------------------------------------------------
 
   std::string_view param(std::string_view key) {
@@ -62,10 +64,20 @@ public:
   ptrdiff_t consume(const net::http::request_header& request_hdr,
                     const_byte_span body) override {
     hdr = request_hdr;
-    auto content = "Hello world!"sv;
-    down->send_response(net::http::status::ok, "text/plain",
-                        as_bytes(make_span(content)));
     payload.assign(body.begin(), body.end());
+    constexpr auto content1 = "Hello world!"sv;
+    constexpr auto content2 = "Developer Network"sv;
+    if (chunked_response) {
+      down->begin_header(net::http::status::ok);
+      down->add_header_field("Transfer-Encoding", "chunked");
+      down->end_header();
+      down->send_chunk(as_bytes(make_span(content1)));
+      down->send_chunk(as_bytes(make_span(content2)));
+      down->send_end_of_chunks();
+    } else {
+      down->send_response(net::http::status::ok, "text/plain",
+                          as_bytes(make_span(content1)));
+    }
     return static_cast<ptrdiff_t>(body.size());
   }
 };
@@ -101,6 +113,37 @@ SCENARIO("the server parses HTTP GET requests into header fields") {
         CHECK_EQ(app->hdr.field("Accept-Encoding"), "gzip");
       }
       AND("the server properly formats a response from the application layer") {
+        CHECK_EQ(serv->output_as_str(), res);
+      }
+    }
+  }
+}
+
+SCENARIO("the client receives a chunked HTTP response") {
+  GIVEN("valid HTTP GET request accepting chunked encoding") {
+    std::string_view req = "GET /foo/bar?user=foo&pw=bar HTTP/1.1\r\n"
+                           "Host: localhost:8090\r\n"
+                           "User-Agent: AwesomeLib/1.0\r\n"
+                           "Accept-Encoding: chunked\r\n\r\n";
+    std::string_view res = "HTTP/1.1 200 OK\r\n"
+                           "Transfer-Encoding: chunked\r\n"
+                           "\r\n"
+                           "C\r\n"
+                           "Hello world!\r\n"
+                           "11\r\n"
+                           "Developer Network\r\n"
+                           "0\r\n"
+                           "\r\n";
+    WHEN("sending it to an HTTP server") {
+      auto app_ptr = app_t::make();
+      auto app = app_ptr.get();
+      auto http_ptr = net::http::server::make(std::move(app_ptr));
+      auto serv = mock_stream_transport::make(std::move(http_ptr));
+      CHECK_EQ(serv->start(nullptr), error{});
+      app->chunked_response = true;
+      serv->push(req);
+      THEN("the HTTP layer sends a chunked response to the client") {
+        CHECK_EQ(serv->handle_input(), static_cast<ptrdiff_t>(req.size()));
         CHECK_EQ(serv->output_as_str(), res);
       }
     }
