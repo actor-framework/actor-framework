c445ba4c97d7cdaf68c67deab669f8f88ad947f7
Dominik Charousset <dominik@charousset.de>
2023-10-12 02:41:54 +0200

    Integrate review feedback

--- libcaf_core/caf/detail/parse.test.cpp
+++ libcaf_core/caf/detail/parse.test.cpp
@@ -56,83 +56,83 @@ expected<T> read(std::string_view str) {
 
 } // namespace
 
-#define check_NUMBER(type, value) check_eq(read<type>(#value), type(value))
+#define CHECK_NUMBER(type, value) check_eq(read<type>(#value), type(value))
 
-#define check_NUMBER_3(type, value, cpp_value)                                 \
+#define CHECK_NUMBER_3(type, value, cpp_value)                                 \
   check_eq(read<type>(#value), type(cpp_value))
 
-#define check_INVALID(type, str, code) check_eq(read<type>(str), code)
+#define CHECK_INVALID(type, str, code) check_eq(read<type>(str), code)
 
 TEST("valid signed integers") {
-  check_NUMBER(int8_t, -128);
-  check_NUMBER(int8_t, 127);
-  check_NUMBER(int8_t, +127);
-  check_NUMBER(int16_t, -32768);
-  check_NUMBER(int16_t, 32767);
-  check_NUMBER(int16_t, +32767);
-  check_NUMBER(int32_t, -2147483648);
-  check_NUMBER(int32_t, 2147483647);
-  check_NUMBER(int32_t, +2147483647);
-  check_NUMBER(int64_t, -9223372036854775807);
-  check_NUMBER(int64_t, 9223372036854775807);
-  check_NUMBER(int64_t, +9223372036854775807);
+  CHECK_NUMBER(int8_t, -128);
+  CHECK_NUMBER(int8_t, 127);
+  CHECK_NUMBER(int8_t, +127);
+  CHECK_NUMBER(int16_t, -32768);
+  CHECK_NUMBER(int16_t, 32767);
+  CHECK_NUMBER(int16_t, +32767);
+  CHECK_NUMBER(int32_t, -2147483648);
+  CHECK_NUMBER(int32_t, 2147483647);
+  CHECK_NUMBER(int32_t, +2147483647);
+  CHECK_NUMBER(int64_t, -9223372036854775807);
+  CHECK_NUMBER(int64_t, 9223372036854775807);
+  CHECK_NUMBER(int64_t, +9223372036854775807);
 }
 
 TEST("invalid signed integers") {
-  check_INVALID(int8_t, "--1", pec::unexpected_character);
-  check_INVALID(int8_t, "++1", pec::unexpected_character);
-  check_INVALID(int8_t, "-129", pec::integer_underflow);
-  check_INVALID(int8_t, "128", pec::integer_overflow);
-  check_INVALID(int8_t, "~1", pec::unexpected_character);
-  check_INVALID(int8_t, "1!", pec::trailing_character);
-  check_INVALID(int8_t, "+", pec::unexpected_eof);
-  check_INVALID(int8_t, "-", pec::unexpected_eof);
+  CHECK_INVALID(int8_t, "--1", pec::unexpected_character);
+  CHECK_INVALID(int8_t, "++1", pec::unexpected_character);
+  CHECK_INVALID(int8_t, "-129", pec::integer_underflow);
+  CHECK_INVALID(int8_t, "128", pec::integer_overflow);
+  CHECK_INVALID(int8_t, "~1", pec::unexpected_character);
+  CHECK_INVALID(int8_t, "1!", pec::trailing_character);
+  CHECK_INVALID(int8_t, "+", pec::unexpected_eof);
+  CHECK_INVALID(int8_t, "-", pec::unexpected_eof);
 }
 
 TEST("valid unsigned integers") {
-  check_NUMBER(uint8_t, 0);
-  check_NUMBER(uint8_t, +0);
-  check_NUMBER(uint8_t, 255);
-  check_NUMBER(uint8_t, +255);
-  check_NUMBER(uint16_t, 0);
-  check_NUMBER(uint16_t, +0);
-  check_NUMBER(uint16_t, 65535);
-  check_NUMBER(uint16_t, +65535);
-  check_NUMBER(uint32_t, 0);
-  check_NUMBER(uint32_t, +0);
-  check_NUMBER(uint32_t, 4294967295);
-  check_NUMBER(uint32_t, +4294967295);
-  check_NUMBER(uint64_t, 0);
-  check_NUMBER(uint64_t, +0);
-  check_NUMBER_3(uint64_t, 18446744073709551615, 18446744073709551615ULL);
-  check_NUMBER_3(uint64_t, +18446744073709551615, 18446744073709551615ULL);
+  CHECK_NUMBER(uint8_t, 0);
+  CHECK_NUMBER(uint8_t, +0);
+  CHECK_NUMBER(uint8_t, 255);
+  CHECK_NUMBER(uint8_t, +255);
+  CHECK_NUMBER(uint16_t, 0);
+  CHECK_NUMBER(uint16_t, +0);
+  CHECK_NUMBER(uint16_t, 65535);
+  CHECK_NUMBER(uint16_t, +65535);
+  CHECK_NUMBER(uint32_t, 0);
+  CHECK_NUMBER(uint32_t, +0);
+  CHECK_NUMBER(uint32_t, 4294967295);
+  CHECK_NUMBER(uint32_t, +4294967295);
+  CHECK_NUMBER(uint64_t, 0);
+  CHECK_NUMBER(uint64_t, +0);
+  CHECK_NUMBER_3(uint64_t, 18446744073709551615, 18446744073709551615ULL);
+  CHECK_NUMBER_3(uint64_t, +18446744073709551615, 18446744073709551615ULL);
 }
 
 TEST("invalid unsigned integers") {
-  check_INVALID(uint8_t, "-1", pec::unexpected_character);
-  check_INVALID(uint8_t, "++1", pec::unexpected_character);
-  check_INVALID(uint8_t, "256", pec::integer_overflow);
-  check_INVALID(uint8_t, "~1", pec::unexpected_character);
-  check_INVALID(uint8_t, "1!", pec::trailing_character);
-  check_INVALID(uint8_t, "+", pec::unexpected_eof);
+  CHECK_INVALID(uint8_t, "-1", pec::unexpected_character);
+  CHECK_INVALID(uint8_t, "++1", pec::unexpected_character);
+  CHECK_INVALID(uint8_t, "256", pec::integer_overflow);
+  CHECK_INVALID(uint8_t, "~1", pec::unexpected_character);
+  CHECK_INVALID(uint8_t, "1!", pec::trailing_character);
+  CHECK_INVALID(uint8_t, "+", pec::unexpected_eof);
 }
 
 TEST("valid floating point numbers") {
-  check_NUMBER(float, 1);
-  check_NUMBER(double, 1);
-  check_NUMBER(double, 0.01e10);
-  check_NUMBER(double, 10e-10);
-  check_NUMBER(double, -10e-10);
+  CHECK_NUMBER(float, 1);
+  CHECK_NUMBER(double, 1);
+  CHECK_NUMBER(double, 0.01e10);
+  CHECK_NUMBER(double, 10e-10);
+  CHECK_NUMBER(double, -10e-10);
 }
 
 TEST("invalid floating point numbers") {
-  check_INVALID(float, "1..", pec::trailing_character);
-  check_INVALID(double, "..1", pec::unexpected_character);
-  check_INVALID(double, "+", pec::unexpected_eof);
-  check_INVALID(double, "-", pec::unexpected_eof);
-  check_INVALID(double, "1e", pec::unexpected_eof);
-  check_INVALID(double, "--0.01e10", pec::unexpected_character);
-  check_INVALID(double, "++10e-10", pec::unexpected_character);
+  CHECK_INVALID(float, "1..", pec::trailing_character);
+  CHECK_INVALID(double, "..1", pec::unexpected_character);
+  CHECK_INVALID(double, "+", pec::unexpected_eof);
+  CHECK_INVALID(double, "-", pec::unexpected_eof);
+  CHECK_INVALID(double, "1e", pec::unexpected_eof);
+  CHECK_INVALID(double, "--0.01e10", pec::unexpected_character);
+  CHECK_INVALID(double, "++10e-10", pec::unexpected_character);
 }
 
 TEST("valid timespans") {
