7cf23fc3e6bc7b1b0b05f8c48069392eaaff46d5
Dominik Charousset <dominik@charousset.de>
2023-11-05 20:27:19 +0100

    Remove unsafe mpx.run(), use URI parameter

--- libcaf_net/tests/drivers/http_client.cpp
+++ libcaf_net/tests/drivers/http_client.cpp
@@ -9,9 +9,11 @@
 #include "caf/actor_system.hpp"
 #include "caf/actor_system_config.hpp"
 #include "caf/caf_main.hpp"
+#include "caf/detail/latch.hpp"
 #include "caf/ipv4_address.hpp"
 
 #include <iostream>
+#include <memory>
 #include <utility>
 
 using namespace std::literals;
@@ -23,14 +25,12 @@ using namespace caf;
 struct config : actor_system_config {
   config() {
     opt_group{custom_options_, "global"}
-      .add<uint16_t>("port,p", "port to connect to")
       .add<http::method>("method,m", "HTTP method to use")
-      .add<std::string>("resource,r", "Requested resource")
+      .add<uri>("resource,r", "Requested resource (URL)")
       .add<std::string>("payload,p", "Optional payload to send");
   }
 };
 
-constexpr auto default_port = uint16_t{8000};
 constexpr auto default_method = http::method::get;
 
 // -- http client sending get request and awaiting response --------------------
@@ -46,7 +46,11 @@ public:
     : method_{method},
       resource_{std::move(resource)},
       payload_{std::move(payload)} {
-    // nop
+    latch_ = std::make_shared<detail::latch>(2);
+  }
+
+  ~http_app() {
+    latch_->count_down();
   }
 
   virtual void prepare_send() override {
@@ -87,29 +91,45 @@ public:
     return static_cast<ptrdiff_t>(payload.size());
   }
 
+  std::shared_ptr<detail::latch> latch() const noexcept {
+    return latch_;
+  }
+
+private:
   http::lower_layer::client* down = nullptr;
   http::method method_;
   std::string resource_;
   std::string payload_;
+  std::shared_ptr<detail::latch> latch_;
 };
 
 int caf_main(caf::actor_system& sys, const config& cfg) {
-  auto port = caf::get_or(cfg, "port", default_port);
-  auto method = caf::get_or(cfg, "method", default_method);
-  auto resource = caf::get_or<std::string>(cfg, "resource", "/");
-  auto payload = caf::get_or<std::string>(cfg, "payload", "");
-  auto sock = caf::net::make_connected_tcp_stream_socket(
-    caf::ip_endpoint{caf::make_ipv4_address(127, 0, 0, 1), port});
-  if (!sock)
+  auto maybe_resource = caf::get_as<uri>(cfg, "resource");
+  if (!maybe_resource) {
+    std::cerr << "*** missing mandatory option --resource" << std::endl;
     return EXIT_FAILURE;
+  }
+  auto resource = std::move(*maybe_resource);
+  if (resource.scheme() != "http") {
+    std::cerr << "*** only HTTP is supported at the moment" << std::endl;
+    return EXIT_FAILURE;
+  }
+  auto method = caf::get_or(cfg, "method", default_method);
+  auto payload = caf::get_or(cfg, "payload", ""sv);
+  auto sock = caf::net::make_connected_tcp_stream_socket(resource.authority());
+  if (!sock) {
+    std::cerr << "*** failed to connect to " << to_string(resource.authority())
+              << std::endl;
+    return EXIT_FAILURE;
+  }
   auto& mpx = sys.network_manager().mpx();
-  auto app = http_app::make(method, resource, payload);
+  auto app = http_app::make(method, resource.path_query_fragment(), payload);
+  auto latch = app->latch();
   auto http_client = http::client::make(std::move(app));
   auto transport
     = caf::net::octet_stream::transport::make(*sock, std::move(http_client));
-  auto mgr = caf::net::socket_manager::make(&mpx, std::move(transport));
-  mpx.start(mgr);
-  mpx.run();
+  mpx.start(caf::net::socket_manager::make(&mpx, std::move(transport)));
+  latch->count_down_and_wait();
   return EXIT_SUCCESS;
 }
 
