d1104a4ddd91dac25cdaf74dcd6ba7f27928783c
Dominik Charousset <dominik@charousset.de>
2023-11-10 18:21:31 +0100

    Migrate typed_stream test and improve coverage

--- libcaf_core/caf/typed_stream.test.cpp
+++ libcaf_core/caf/typed_stream.test.cpp
@@ -2,7 +2,7 @@
 // the main distribution directory for license terms and copyright or visit
 // https://github.com/actor-framework/actor-framework/blob/master/LICENSE.
 
-#include "caf/stream.hpp"
+#include "caf/typed_stream.hpp"
 
 #include "caf/test/caf_test_main.hpp"
 #include "caf/test/fixture/deterministic.hpp"
@@ -16,16 +16,23 @@
 using namespace caf;
 using namespace std::literals;
 
+// Some offset to avoid collisions with other type IDs.
+CAF_BEGIN_TYPE_ID_BLOCK(typed_stream_test, caf::first_custom_type_id + 20)
+  CAF_ADD_TYPE_ID(typed_stream_test, (caf::typed_stream<int32_t>) )
+CAF_END_TYPE_ID_BLOCK(typed_stream_test)
+
 namespace {
 
 using ivec = std::vector<int>;
 
+using stream_type = typed_stream<int32_t>;
+
 behavior int_sink(event_based_actor* self, std::shared_ptr<ivec> results) {
   caf::logger::debug("stream.test", "started sink with ID {}", self->id());
   return {
-    [self, results](const stream& input) {
+    [self, results](const stream_type& input) {
       self //
-        ->observe_as<int>(input, 30, 10)
+        ->observe(input, 30, 10)
         .do_finally([self] {
           caf::logger::debug("stream.test", "sink with ID {} shuts down",
                              self->id());
@@ -39,11 +46,8 @@ behavior int_sink(event_based_actor* self, std::shared_ptr<ivec> results) {
 WITH_FIXTURE(test::fixture::deterministic) {
 
 TEST("default-constructed streams are invalid") {
-  auto uut = stream{};
-  check(!uut.has_element_type<int32_t>());
+  auto uut = stream_type{};
   check_eq(uut.id(), 0u);
-  check_eq(uut.type(), invalid_type_id);
-  check_eq(uut.name(), "");
   check_eq(uut.source(), nullptr);
   check_eq(uut, serialization_roundtrip(uut));
 }
@@ -53,11 +57,8 @@ TEST("streams are serializable") {
   // Note: we need to terminate the dummy actor manually because the registry
   //       holds a reference to it when we serialize it.
   auto dummy_guard = make_actor_scope_guard(dummy);
-  auto uut = stream{actor_cast<strong_actor_ptr>(dummy), type_id_v<int32_t>,
-                    "foo", 42};
-  check(uut.has_element_type<int32_t>());
+  auto uut = stream_type{actor_cast<strong_actor_ptr>(dummy), "foo", 42};
   check_eq(uut.id(), 42u);
-  check_eq(uut.type(), type_id_v<int32_t>);
   check_eq(uut.name(), "foo");
   check_eq(uut.source(), dummy);
   check_eq(uut, serialization_roundtrip(uut));
@@ -66,16 +67,14 @@ TEST("streams are serializable") {
 TEST("streams are comparable") {
   auto dummy1 = sys.spawn([] { return behavior{[](int) {}}; });
   auto dummy2 = sys.spawn([] { return behavior{[](int) {}}; });
-  stream s1;
-  stream s2;
+  stream_type s1;
+  stream_type s2;
   if (dummy1 > dummy2) {
     std::swap(dummy1, dummy2);
   }
   SECTION("streams with different sources are not equal") {
-    s1 = stream{actor_cast<strong_actor_ptr>(dummy1), type_id_v<int32_t>, "foo",
-                42};
-    s2 = stream{actor_cast<strong_actor_ptr>(dummy2), type_id_v<int32_t>, "foo",
-                42};
+    s1 = stream_type{actor_cast<strong_actor_ptr>(dummy1), "foo", 42};
+    s2 = stream_type{actor_cast<strong_actor_ptr>(dummy2), "foo", 42};
     check_ne(s1, s2);
     check_lt(s1, s2);
     check_le(s1, s2);
@@ -83,19 +82,15 @@ TEST("streams are comparable") {
     check_ge(s2, s1);
   }
   SECTION("streams with the same source and ID are equal") {
-    s1 = stream{actor_cast<strong_actor_ptr>(dummy1), type_id_v<int32_t>, "foo",
-                42};
-    s2 = stream{actor_cast<strong_actor_ptr>(dummy1), type_id_v<int32_t>, "foo",
-                42};
+    s1 = stream_type{actor_cast<strong_actor_ptr>(dummy1), "foo", 42};
+    s2 = stream_type{actor_cast<strong_actor_ptr>(dummy1), "foo", 42};
     check_eq(s1, s2);
     check_le(s1, s2);
     check_ge(s2, s1);
   }
   SECTION("streams to the same source are sorted by ID") {
-    s1 = stream{actor_cast<strong_actor_ptr>(dummy1), type_id_v<int32_t>, "foo",
-                42};
-    s2 = stream{actor_cast<strong_actor_ptr>(dummy1), type_id_v<int32_t>, "bar",
-                84};
+    s1 = stream_type{actor_cast<strong_actor_ptr>(dummy1), "foo", 42};
+    s2 = stream_type{actor_cast<strong_actor_ptr>(dummy1), "bar", 84};
     check_ne(s1, s2);
     check_lt(s1, s2);
     check_le(s1, s2);
@@ -116,14 +111,14 @@ TEST("streams allow actors to transmit flow items to other actors") {
     auto src = sys.spawn([s1, s2](event_based_actor* self) {
       auto vals = self //
                     ->make_observable()
-                    .iota(1)
+                    .iota(int32_t{1})
                     .take(256)
-                    .to_stream("foo", 10ms, 10);
+                    .to_typed_stream("foo", 10ms, 10);
       self->send(s1, vals);
       self->send(s2, vals);
     });
-    expect<stream>().from(src).to(s1);
-    expect<stream>().from(src).to(s2);
+    expect<stream_type>().from(src).to(s1);
+    expect<stream_type>().from(src).to(s2);
     expect<stream_open_msg>().from(s1).to(src);
     expect<stream_open_msg>().from(s2).to(src);
     expect<stream_ack_msg>().from(src).to(s1);
@@ -139,12 +134,12 @@ TEST("streams allow actors to transmit flow items to other actors") {
     auto src = sys.spawn([s1](event_based_actor* self) {
       auto vals = self //
                     ->make_observable()
-                    .iota(1)
+                    .iota(int32_t{1})
                     .take(256)
-                    .to_stream("foo", 10ms, 10);
+                    .to_typed_stream("foo", 10ms, 10);
       self->send(s1, vals);
     });
-    expect<stream>().from(src).to(s1);
+    expect<stream_type>().from(src).to(s1);
     expect<stream_open_msg>().from(s1).to(src);
     expect<stream_ack_msg>().from(src).to(s1);
     inject_exit(src);
@@ -157,12 +152,12 @@ TEST("streams allow actors to transmit flow items to other actors") {
     auto src = sys.spawn([s1](event_based_actor* self) {
       auto vals = self //
                     ->make_observable()
-                    .iota(1)
+                    .iota(int32_t{1})
                     .take(256)
-                    .to_stream("foo", 10ms, 10);
+                    .to_typed_stream("foo", 10ms, 10);
       self->send(s1, vals);
     });
-    expect<stream>().from(src).to(s1);
+    expect<stream_type>().from(src).to(s1);
     expect<stream_open_msg>().from(s1).to(src);
     expect<stream_ack_msg>().from(src).to(s1);
     inject_exit(s1);
@@ -173,5 +168,9 @@ TEST("streams allow actors to transmit flow items to other actors") {
 }
 
 } // WITH_FIXTURE(test::fixture::deterministic)
+
+TEST_INIT() {
+  init_global_meta_objects<id_block::typed_stream_test>();
+}
 
 } // namespace
