105d800aa06140188eadc9c28c7f810e20eb4b8c
Dominik Charousset <dominik@charousset.de>
2024-01-21 15:52:41 +0100

    Build DLLs on Windows CI
    (cherry picked from commit f8a0f226ec6feda2ea1bbac3946eac02f7365bb9)

--- .ci/windows/build.cmd
+++ .ci/windows/build.cmd
@@ -5,10 +5,10 @@ cmake.exe ^
   -C ".ci\debug-flags.cmake" ^
   -DCAF_ENABLE_ROBOT_TESTS=ON ^
   -DCAF_LOG_LEVEL=TRACE ^
-  -DBUILD_SHARED_LIBS=OFF ^
   -DCMAKE_C_COMPILER=cl.exe ^
   -DCMAKE_CXX_COMPILER=cl.exe ^
   -DCMAKE_CXX_FLAGS="/WX" ^
-  -DPython3_ROOT_DIR=%Python3_ROOT_DIR%
+  -DPython3_ROOT_DIR=%Python3_ROOT_DIR% ^
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=lib
 
 cmake.exe --build build --parallel %NUMBER_OF_PROCESSORS% --target install --config debug || exit \b 1
