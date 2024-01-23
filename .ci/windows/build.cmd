93546d4527a616e26c213167449d866b30671e99
Dominik Charousset <dominik@charousset.de>
2024-01-23 08:01:09 +0100

    Adjust build flags for Windows CI

--- .ci/windows/build.cmd
+++ .ci/windows/build.cmd
@@ -3,12 +3,11 @@ cmake.exe ^
   -B build ^
   -G "Visual Studio 17 2022" ^
   -C ".ci\debug-flags.cmake" ^
-  -DCAF_ENABLE_ROBOT_TESTS=ON ^
-  -DCAF_LOG_LEVEL=TRACE ^
   -DCMAKE_C_COMPILER=cl.exe ^
   -DCMAKE_CXX_COMPILER=cl.exe ^
   -DCMAKE_CXX_FLAGS="/WX" ^
   -DPython3_ROOT_DIR=%Python3_ROOT_DIR% ^
-  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=lib
+  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=%cd%\bin ^
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=%cd%\bin
 
 cmake.exe --build build --parallel %NUMBER_OF_PROCESSORS% --target install --config debug || exit \b 1
