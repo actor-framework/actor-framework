2b74b053ebd69d697686fca7c172806a36b5bd02
Dominik Charousset <dominik@charousset.de>
2024-01-23 08:01:09 +0100

    Adjust build flags for Windows CI
    (cherry picked from commit 93546d4527a616e26c213167449d866b30671e99)

--- .ci/windows/test.cmd
+++ .ci/windows/test.cmd
@@ -1,2 +1,2 @@
-set PATH=%cd%\lib\Debug;%PATH%
-ctest --output-on-failure -C debug --test-dir build || exit \b 1
+set PATH=%cd%\bin\Debug;%PATH%
+ctest.exe --output-on-failure -C debug --test-dir build || exit \b 1
