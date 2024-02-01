939d6d77e016aa3bc782ebd05c73281bf52b9336
Dominik Charousset <dominik@charousset.de>
2024-02-01 13:08:20 +0100

    Improve coverage for `actor_from_state`

--- libcaf_core/caf/actor_from_state.test.cpp
+++ libcaf_core/caf/actor_from_state.test.cpp
@@ -7,6 +7,7 @@
 #include "caf/test/fixture/deterministic.hpp"
 #include "caf/test/test.hpp"
 
+#include "caf/event_based_actor.hpp"
 #include "caf/typed_actor.hpp"
 
 using namespace caf;
@@ -101,6 +102,18 @@ TEST("passing a value to the cell constructor overrides the default value") {
     inject().with(get_atom_v).from(dummy).to(uut);
     expect<int>().with(23).from(uut).to(dummy);
   }
+}
+
+TEST("actors can spawn stateful actors as children") {
+  auto dummy = sys.spawn(dummy_impl);
+  auto [parent, run_parent] = sys.spawn_inactive<event_based_actor>();
+  auto uut = parent->spawn(actor_from_state<cell_state>, 42);
+  static_assert(std::is_same_v<decltype(uut), actor>);
+  inject().with(get_atom_v).from(dummy).to(uut);
+  expect<int>().with(42).from(uut).to(dummy);
+  inject().with(put_atom_v, 23).from(dummy).to(uut);
+  inject().with(get_atom_v).from(dummy).to(uut);
+  expect<int>().with(23).from(uut).to(dummy);
 }
 
 struct id_cell_state {
