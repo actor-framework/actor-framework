dba61d648e4bbbf8fa202347b47eaf6a4d0d5185
Samir Halilcevic <s.halilcevic@interance.io>
2024-03-20 12:54:22 +0100

    Hide the implementation of net::web_socket::client

--- libcaf_net/caf/net/web_socket/client.hpp
+++ libcaf_net/caf/net/web_socket/client.hpp
@@ -5,22 +5,8 @@
 #pragma once
 
 #include "caf/net/fwd.hpp"
-#include "caf/net/http/v1.hpp"
 #include "caf/net/octet_stream/upper_layer.hpp"
-#include "caf/net/receive_policy.hpp"
-#include "caf/net/web_socket/framing.hpp"
 #include "caf/net/web_socket/handshake.hpp"
-
-#include "caf/byte_span.hpp"
-#include "caf/detail/base64.hpp"
-#include "caf/detail/message_flow_bridge.hpp"
-#include "caf/error.hpp"
-#include "caf/hash/sha1.hpp"
-#include "caf/logger.hpp"
-#include "caf/pec.hpp"
-#include "caf/settings.hpp"
-
-#include <algorithm>
 
 namespace caf::net::web_socket {
 
@@ -36,13 +22,6 @@ public:
 
   using upper_layer_ptr = std::unique_ptr<web_socket::upper_layer>;
 
-  // -- constructors, destructors, and assignment operators --------------------
-
-  client(handshake_ptr hs, upper_layer_ptr up)
-    : hs_(std::move(hs)), up_(std::move(up)) {
-    // nop
-  }
-
   // -- factories --------------------------------------------------------------
 
   static std::unique_ptr<client> make(handshake_ptr hs, upper_layer_ptr up);
@@ -50,34 +29,6 @@ public:
   static std::unique_ptr<client> make(handshake&& hs, upper_layer_ptr up) {
     return make(std::make_unique<handshake>(std::move(hs)), std::move(up));
   }
-
-  // -- implementation of octet_stream::upper_layer ----------------------------
-
-  error start(octet_stream::lower_layer* down) override;
-
-  void abort(const error& reason) override;
-
-  ptrdiff_t consume(byte_span buffer, byte_span delta) override;
-
-  void prepare_send() override;
-
-  bool done_sending() override;
-
-private:
-  // -- HTTP response processing -----------------------------------------------
-
-  bool handle_header(std::string_view http);
-
-  // -- member variables -------------------------------------------------------
-
-  /// Points to the transport layer below.
-  octet_stream::lower_layer* down_;
-
-  /// Stores the WebSocket handshake data until the handshake completed.
-  handshake_ptr hs_;
-
-  /// Next layer in the processing chain.
-  upper_layer_ptr up_;
 };
 
 } // namespace caf::net::web_socket
