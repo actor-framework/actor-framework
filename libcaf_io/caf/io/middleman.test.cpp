30562f7593f5d6d5f5778b62606240d414654798
Samir Halilcevic <s.halilcevic@interance.io>
2024-07-01 16:39:15 +0200

    Integrate review feedback

--- libcaf_io/caf/io/middleman.test.cpp
+++ libcaf_io/caf/io/middleman.test.cpp
@@ -12,15 +12,18 @@ using namespace caf::io;
 
 namespace {
 
-// Regression test for GH-#1900.
-// Note: we can't reproduce this in compile time,
-// static_assert(std::is_same_v<..snippet.., expected<actor>>) won't work.
-TEST("spawn client works") {
-  caf::actor_system_config cfg;
-  cfg.load<io::middleman>();
-  caf::actor_system sys{cfg};
-  sys.middleman().spawn_client(
-    [](caf::io::broker*, caf::io::connection_handle) {}, "foo", 42);
+TEST("GH-1900 regression") {
+  // Note: we don't need to actually run `spawn_client`, we only check for a
+  // compiler error. We shouldn't actually run this code, since it will assume
+  // 42 is a socket descriptor.
+  auto fn = [] {
+    caf::actor_system_config cfg;
+    cfg.load<io::middleman>();
+    caf::actor_system sys{cfg};
+    sys.middleman().spawn_client(
+      [](caf::io::broker*, caf::io::connection_handle) {}, "foo", 42);
+  };
+  check(std::is_same_v<decltype(fn), decltype(fn)>);
 }
 
 } // namespace
