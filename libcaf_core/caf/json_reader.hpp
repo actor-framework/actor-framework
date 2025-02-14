0f465b6797ba587da4024c6eff3118004b9ab6b0
Shariar Azad Riday <s.riday@interance.io>
2025-02-15 01:09:52 +0600

    Use fast_pimpl idiom for json_reader

--- libcaf_core/caf/json_reader.hpp
+++ libcaf_core/caf/json_reader.hpp
@@ -6,85 +6,15 @@
 
 #include "caf/byte_reader.hpp"
 #include "caf/detail/core_export.hpp"
-#include "caf/detail/json.hpp"
+#include "caf/fwd.hpp"
 
-#include <iosfwd>
-#include <string_view>
-#include <variant>
+#include <cstddef>
 
 namespace caf {
 
 /// Deserializes an inspectable object from a JSON-formatted string.
-class CAF_CORE_EXPORT json_reader : public byte_reader {
+class CAF_CORE_EXPORT json_reader final : public byte_reader {
 public:
-  // -- member types -----------------------------------------------------------
-
-  using super = byte_reader;
-
-  struct sequence {
-    detail::json::array::const_iterator pos;
-
-    detail::json::array::const_iterator end;
-
-    bool at_end() const noexcept {
-      return pos == end;
-    }
-
-    auto& current() {
-      return *pos;
-    }
-
-    void advance() {
-      ++pos;
-    }
-  };
-
-  struct members {
-    detail::json::object::const_iterator pos;
-
-    detail::json::object::const_iterator end;
-
-    bool at_end() const noexcept {
-      return pos == end;
-    }
-
-    auto& current() {
-      return *pos;
-    }
-
-    void advance() {
-      ++pos;
-    }
-  };
-
-  using json_key = std::string_view;
-
-  using value_type
-    = std::variant<const detail::json::value*, const detail::json::object*,
-                   detail::json::null_t, json_key, sequence, members>;
-
-  using stack_allocator
-    = detail::monotonic_buffer_resource::allocator<value_type>;
-
-  using stack_type = std::vector<value_type, stack_allocator>;
-
-  /// Denotes the type at the current position.
-  enum class position {
-    value,
-    object,
-    null,
-    key,
-    sequence,
-    members,
-    past_the_end,
-    invalid,
-  };
-
-  // -- constants --------------------------------------------------------------
-
-  /// The value value for `field_type_suffix()`.
-  static constexpr std::string_view field_type_suffix_default = "-type";
-
   // -- constructors, destructors, and assignment operators --------------------
 
   json_reader();
@@ -102,24 +32,16 @@ public:
   /// Returns the suffix for generating type annotation fields for variant
   /// fields. For example, CAF inserts field called "@foo${field_type_suffix}"
   /// for a variant field called "foo".
-  [[nodiscard]] std::string_view field_type_suffix() const noexcept {
-    return field_type_suffix_;
-  }
+  [[nodiscard]] std::string_view field_type_suffix() const noexcept;
 
   /// Configures whether the writer omits empty fields.
-  void field_type_suffix(std::string_view suffix) noexcept {
-    field_type_suffix_ = suffix;
-  }
+  void field_type_suffix(std::string_view suffix) noexcept;
 
   /// Returns the type ID mapper used by the writer.
-  [[nodiscard]] const type_id_mapper* mapper() const noexcept {
-    return mapper_;
-  }
+  [[nodiscard]] const type_id_mapper* mapper() const noexcept;
 
   /// Changes the type ID mapper for the writer.
-  void mapper(const type_id_mapper* ptr) noexcept {
-    mapper_ = ptr;
-  }
+  void mapper(const type_id_mapper* ptr) noexcept;
 
   // -- modifiers --------------------------------------------------------------
 
@@ -151,9 +73,7 @@ public:
   bool load_file(const char* path);
 
   /// @copydoc load_file
-  bool load_file(const std::string& path) {
-    return load_file(path.c_str());
-  }
+  bool load_file(const std::string& path);
 
   /// Reverts the state of the reader back to where it was after calling `load`.
   /// @post The reader is ready for attempting to deserialize another
@@ -240,57 +160,8 @@ public:
   bool value(span<std::byte> x) override;
 
 private:
-  [[nodiscard]] position pos() const noexcept;
-
-  void append_current_field_name(std::string& str);
-
-  std::string current_field_name();
-
-  template <bool PopOrAdvanceOnSuccess, class F>
-  bool consume(const char* fun_name, F f);
-
-  template <class T>
-  bool integer(T& x);
-
-  template <position P>
-  auto& top() noexcept {
-    return std::get<static_cast<size_t>(P)>(st_->back());
-  }
-
-  template <position P>
-  const auto& top() const noexcept {
-    return std::get<static_cast<size_t>(P)>(st_->back());
-  }
-
-  void pop() {
-    st_->pop_back();
-  }
-
-  template <class T>
-  void push(T&& x) {
-    st_->emplace_back(std::forward<T>(x));
-  }
-
-  actor_system* sys_ = nullptr;
-
-  detail::monotonic_buffer_resource buf_;
-
-  stack_type* st_ = nullptr;
-
-  detail::json::value* root_ = nullptr;
-
-  std::string_view field_type_suffix_ = field_type_suffix_default;
-
-  /// Keeps track of the current field for better debugging output.
-  std::vector<std::string_view> field_;
-
-  /// The mapper implementation we use by default.
-  default_type_id_mapper default_mapper_;
-
-  /// Configures which ID mapper we use to translate between type IDs and names.
-  const type_id_mapper* mapper_ = &default_mapper_;
-
-  error err_;
+  /// Storage for the implementation object.
+  alignas(std::max_align_t) std::byte impl_[224];
 };
 
 } // namespace caf
