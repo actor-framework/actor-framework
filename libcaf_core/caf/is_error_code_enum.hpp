8e5acf569e5d9a7bed2d79412a2a486f571193a2
Dominik Charousset <dominik@charousset.de>
2025-08-11 19:35:34 +0200

    Add is_error_code_enum.hpp as deprecated header

---
+++ libcaf_core/caf/is_error_code_enum.hpp
@@ -0,0 +1,12 @@
+// This file is part of CAF, the C++ Actor Framework. See the file LICENSE in
+// the main distribution directory for license terms and copyright or visit
+// https://github.com/actor-framework/actor-framework/blob/main/LICENSE.
+
+#pragma once
+
+// TODO: remove this file in CAF 3.0.0.
+
+#pragma message("The header 'caf/is_error_code_enum.hpp' is deprecated. "      \
+                "Please include 'caf/error_code_enum.hpp' instead.")
+
+#include "caf/error_code_enum.hpp"
