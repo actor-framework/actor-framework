c97a28f9a2389fb4683d9cd739cde8fefa0f3102
Dominik Charousset <dominik@charousset.de>
2026-02-18 19:48:18 +0100

    Extend testing DSL for metric checks

--- libcaf_test/caf/test/runnable.cpp
+++ libcaf_test/caf/test/runnable.cpp
@@ -5,15 +5,15 @@
 #include "caf/test/runnable.hpp"
 
 #include "caf/test/block_type.hpp"
-#include "caf/test/context.hpp"
 #include "caf/test/outline.hpp"
 #include "caf/test/scenario.hpp"
-#include "caf/test/scope.hpp"
 #include "caf/test/test.hpp"
 
 #include "caf/detail/format.hpp"
 #include "caf/detail/scope_guard.hpp"
+#include "caf/telemetry/metric_registry.hpp"
 
+#include <chrono>
 #include <regex>
 
 namespace caf::test {
@@ -22,36 +22,130 @@ namespace {
 
 thread_local runnable* current_runnable;
 
+std::string render_label_name(std::string_view prefix, std::string_view name,
+                              std::span<const telemetry::label_view> labels) {
+  std::string result = detail::format("{}.{}", prefix, name);
+  if (!labels.empty()) {
+    result += "{";
+    for (const auto& label : labels) {
+      result += label.name();
+      result += "=";
+      result += label.value();
+      result += ",";
+    }
+    result.back() = '}';
+  }
+  return result;
+}
+
 } // namespace
+
+struct runnable::runnable_state {
+  runnable_state(context_ptr ctx, std::string_view description,
+                 block_type root_type, const std::source_location& loc)
+    : ctx_(ctx), description_(description), root_type_(root_type), loc_(loc) {
+    // nop
+  }
+
+  context_ptr ctx_;
+  std::string_view description_;
+  block_type root_type_;
+  std::source_location loc_;
+  const telemetry::metric_registry* current_registry_ = nullptr;
+  timespan registry_poll_interval_ = std::chrono::milliseconds{10};
+  timespan registry_poll_timeout_ = std::chrono::seconds{1};
+};
+
+runnable::runnable(context_ptr ctx, std::string_view description,
+                   block_type root_type, const std::source_location& loc) {
+  if (!ctx) {
+    CAF_RAISE_ERROR(std::invalid_argument, "context cannot be null");
+  }
+  test_state_.reset(
+    new runnable_state(std::move(ctx), description, root_type, loc));
+}
 
 runnable::~runnable() {
   // nop
 }
 
-void runnable::run() {
+/// Sets the thread-local metric registry.
+void runnable::current_metric_registry(const telemetry::metric_registry* ptr) {
+  test_state_->current_registry_ = ptr;
+}
+
+const telemetry::metric_registry*
+runnable::current_metric_registry() const noexcept {
+  return test_state_->current_registry_;
+}
+
+void runnable::metric_registry_poll_interval(timespan interval) {
+  if (interval.count() <= 0) {
+    CAF_RAISE_ERROR(std::invalid_argument, "interval must be positive");
+  }
+  test_state_->registry_poll_interval_ = interval;
+}
+
+timespan runnable::metric_registry_poll_interval() const noexcept {
+  return test_state_->registry_poll_interval_;
+}
+
+void runnable::metric_registry_poll_timeout(timespan timeout) {
+  if (timeout.count() <= 0) {
+    CAF_RAISE_ERROR(std::invalid_argument, "timeout must be positive");
+  }
+  test_state_->registry_poll_timeout_ = timeout;
+}
+
+timespan runnable::metric_registry_poll_timeout() const noexcept {
+  return test_state_->registry_poll_timeout_;
+}
+
+caf::test::context& runnable::test_context() const noexcept {
+  return *test_state_->ctx_;
+}
+
+std::string_view runnable::test_description() const noexcept {
+  return test_state_->description_;
+}
+
+const std::source_location& runnable::test_location() const noexcept {
+  return test_state_->loc_;
+}
+
+void runnable::run_next_test_branch() {
   current_runnable = this;
-  auto guard
-    = detail::scope_guard{[]() noexcept { current_runnable = nullptr; }};
-  switch (root_type_) {
+  detail::scope_guard run_guard{[]() noexcept { current_runnable = nullptr; }};
+  run_next_test_branch_init();
+  switch (test_state_->root_type_) {
     case block_type::outline:
-      if (auto guard = ctx_->get<outline>(0, description_, loc_)->commit()) {
-        do_run();
+      if (auto guard = test_state_->ctx_
+                         ->get<outline>(0, test_state_->description_,
+                                        test_state_->loc_)
+                         ->commit()) {
+        run_next_test_branch_impl();
         return;
       }
       CAF_RAISE_ERROR(std::logic_error,
-                      "failed to select  the root block for the outline");
+                      "failed to select the root block for the outline");
       break;
     case block_type::scenario:
-      if (auto guard = ctx_->get<scenario>(0, description_, loc_)->commit()) {
-        do_run();
+      if (auto guard = test_state_->ctx_
+                         ->get<scenario>(0, test_state_->description_,
+                                         test_state_->loc_)
+                         ->commit()) {
+        run_next_test_branch_impl();
         return;
       }
       CAF_RAISE_ERROR(std::logic_error,
-                      "failed to select  the root block for the scenario");
+                      "failed to select the root block for the scenario");
       break;
     case block_type::test:
-      if (auto guard = ctx_->get<test>(0, description_, loc_)->commit()) {
-        do_run();
+      if (auto guard = test_state_->ctx_
+                         ->get<test>(0, test_state_->description_,
+                                     test_state_->loc_)
+                         ->commit()) {
+        run_next_test_branch_impl();
         return;
       }
       CAF_RAISE_ERROR(std::logic_error,
@@ -62,11 +156,8 @@ void runnable::run() {
   }
 }
 
-void runnable::call_do_run() {
-  current_runnable = this;
-  auto guard
-    = detail::scope_guard{[]() noexcept { current_runnable = nullptr; }};
-  do_run();
+void runnable::run_next_test_branch_init() {
+  // customization point for runnable_with_examples
 }
 
 bool runnable::check(bool value, const std::source_location& location) {
@@ -104,9 +195,113 @@ runnable& runnable::current() {
 }
 
 block& runnable::current_block() {
-  if (ctx_->call_stack.empty())
+  if (test_state_->ctx_->call_stack.empty())
     CAF_RAISE_ERROR(std::logic_error, "no current block");
-  return *ctx_->call_stack.back();
+  return *test_state_->ctx_->call_stack.back();
+}
+
+namespace {
+
+template <class Range1, class Range2>
+bool labels_match(const Range1& want, const Range2& found) {
+  if (want.size() != found.size()) {
+    return false;
+  }
+  for (const auto& lbl : want) {
+    auto is_match = [&lbl](const auto& other) {
+      return lbl.name() == other.name() && lbl.value() == other.value();
+    };
+    if (std::ranges::none_of(found, is_match)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+} // namespace
+
+bool runnable::do_check_metric(std::string_view prefix, std::string_view name,
+                               std::span<const telemetry::label_view> labels,
+                               callback<bool(int64_t)>& pred,
+                               const std::source_location& location) {
+  using namespace std::literals;
+  if (!test_state_->current_registry_) {
+    CAF_RAISE_ERROR(std::logic_error, "no metric registry set");
+  }
+  auto result = test_state_->current_registry_->wait_for(
+    prefix, name, labels, test_state_->registry_poll_timeout_,
+    test_state_->registry_poll_interval_, pred);
+  if (result) {
+    reporter::instance().pass(location);
+    return true;
+  }
+  auto label_name = render_label_name(prefix, name, labels);
+  std::string msg;
+  auto collector = [prefix, name, labels, &label_name,
+                    &msg]<class Wrapped>(const telemetry::metric_family* family,
+                                         const telemetry::metric* instance,
+                                         const Wrapped* wrapped) {
+    if (family->prefix() == prefix && family->name() == name
+        && labels_match(labels, instance->labels())) {
+      if constexpr (detail::one_of<Wrapped, telemetry::int_counter,
+                                   telemetry::int_gauge>) {
+        msg = detail::format("metric {} has value {}", label_name,
+                             wrapped->value());
+      } else {
+        msg = detail::format("metric {} has an unexpected type: "
+                             "expected an integer gauge or counter",
+                             label_name);
+      }
+    }
+  };
+  test_state_->current_registry_->collect(collector);
+  if (msg.empty()) {
+    msg = detail::format("metric {} does not exist", label_name);
+  }
+  reporter::instance().fail(msg, location);
+  return false;
+}
+
+bool runnable::do_check_metric(std::string_view prefix, std::string_view name,
+                               std::span<const telemetry::label_view> labels,
+                               callback<bool(double)>& pred,
+                               const std::source_location& location) {
+  using namespace std::literals;
+  if (!test_state_->current_registry_) {
+    CAF_RAISE_ERROR(std::logic_error, "no metric registry set");
+  }
+  auto result = test_state_->current_registry_->wait_for(
+    prefix, name, labels, test_state_->registry_poll_timeout_,
+    test_state_->registry_poll_interval_, pred);
+  if (result) {
+    reporter::instance().pass(location);
+    return true;
+  }
+  auto label_name = render_label_name(prefix, name, labels);
+  std::string msg;
+  auto collector = [prefix, name, labels, &label_name,
+                    &msg]<class Wrapped>(const telemetry::metric_family* family,
+                                         const telemetry::metric* instance,
+                                         const Wrapped* wrapped) {
+    if (family->prefix() == prefix && family->name() == name
+        && labels_match(labels, instance->labels())) {
+      if constexpr (detail::one_of<Wrapped, telemetry::dbl_counter,
+                                   telemetry::dbl_gauge>) {
+        msg = detail::format("metric {} has value {}", label_name,
+                             wrapped->value());
+      } else {
+        msg = detail::format("metric {} has an unexpected type: "
+                             "expected a double gauge or counter",
+                             label_name);
+      }
+    }
+  };
+  test_state_->current_registry_->collect(collector);
+  if (msg.empty()) {
+    msg = detail::format("metric {} does not exist", label_name);
+  }
+  reporter::instance().fail(msg, location);
+  return false;
 }
 
 } // namespace caf::test
--- libcaf_test/caf/test/test.test.cpp
+++ libcaf_test/caf/test/test.test.cpp
@@ -3,84 +3,122 @@
 
 #include "caf/test/test.hpp"
 
+#include "caf/test/approx.hpp"
 #include "caf/test/requirement_failed.hpp"
 
 #include "caf/log/test.hpp"
+#include "caf/telemetry/metric_registry.hpp"
 
-using caf::test::block_type;
+#include <chrono>
+
+using namespace std::literals;
+
+namespace {
+
+constexpr auto apx1 = caf::test::approx{1.0};
+constexpr auto apx7 = caf::test::approx{7.0};
+
+} // namespace
 
 TEST("tests can contain different types of checks") {
   auto& rep = caf::test::reporter::instance();
   SECTION("check_ne checks for inequality") {
     check_ne(0, 1);
-    should_fail([this]() { check_ne(0, 0); });
+    should_fail([this] { check_ne(0, 0); });
   }
   SECTION("check_eq checks for equality") {
     check_eq(1, 1);
-    should_fail([this]() { check_eq(1, 0); });
+    should_fail([this] { check_eq(1, 0); });
   }
   SECTION("check_ge checks that lhs is greater than or equal to rhs") {
     check_ge(0, 0);
     check_ge(2, 1);
-    should_fail([this]() { check_ge(1, 2); });
+    should_fail([this] { check_ge(1, 2); });
   }
   SECTION("check_gt checks that lhs is greater than rhs") {
     check_gt(2, 1);
-    should_fail([this]() { check_gt(0, 0); });
-    should_fail([this]() { check_gt(1, 2); });
+    should_fail([this] { check_gt(0, 0); });
+    should_fail([this] { check_gt(1, 2); });
   }
   SECTION("check_le checks that lhs is less than or equal to rhs") {
     check_le(0, 0);
     check_le(1, 2);
-    should_fail([this]() { check_le(2, 1); });
+    should_fail([this] { check_le(2, 1); });
   }
   SECTION("check_lt checks that lhs is less than rhs") {
     check_lt(1, 2);
-    should_fail([this]() { check_lt(1, 1); });
-    should_fail([this]() { check_lt(2, 1); });
+    should_fail([this] { check_lt(1, 1); });
+    should_fail([this] { check_lt(2, 1); });
   }
   caf::log::test::debug("this test had {} checks", rep.test_stats().total());
 }
 
 #ifdef CAF_ENABLE_EXCEPTIONS
 
+TEST("all metric checks throw when not setting a registry") {
+  check_throws([this] { check_metric_eq("tst", "name", 1); });
+  check_throws([this] { check_metric_eq("tst", "name", {{"a", "b"}}, 1); });
+  check_throws([this] { check_metric_eq("tst", "name", apx1); });
+  check_throws([this] { check_metric_eq("tst", "name", {{"a", "b"}}, apx1); });
+  check_throws([this] { check_metric_ne("tst", "name", 1); });
+  check_throws([this] { check_metric_ne("tst", "name", {{"a", "b"}}, 1); });
+  check_throws([this] { check_metric_ne("tst", "name", apx1); });
+  check_throws([this] { check_metric_ne("tst", "name", {{"a", "b"}}, apx1); });
+  check_throws([this] { check_metric_lt("tst", "name", 1); });
+  check_throws([this] { check_metric_lt("tst", "name", {{"a", "b"}}, 1); });
+  check_throws([this] { check_metric_lt("tst", "name", 1.0); });
+  check_throws([this] { check_metric_lt("tst", "name", {{"a", "b"}}, 1.0); });
+  check_throws([this] { check_metric_le("tst", "name", 1); });
+  check_throws([this] { check_metric_le("tst", "name", {{"a", "b"}}, 1); });
+  check_throws([this] { check_metric_le("tst", "name", 1.0); });
+  check_throws([this] { check_metric_le("tst", "name", {{"a", "b"}}, 1.0); });
+  check_throws([this] { check_metric_gt("tst", "name", 1); });
+  check_throws([this] { check_metric_gt("tst", "name", {{"a", "b"}}, 1); });
+  check_throws([this] { check_metric_gt("tst", "name", 1.0); });
+  check_throws([this] { check_metric_gt("tst", "name", {{"a", "b"}}, 1.0); });
+  check_throws([this] { check_metric_ge("tst", "name", 1); });
+  check_throws([this] { check_metric_ge("tst", "name", {{"a", "b"}}, 1); });
+  check_throws([this] { check_metric_ge("tst", "name", 1.0); });
+  check_throws([this] { check_metric_ge("tst", "name", {{"a", "b"}}, 1.0); });
+}
+
 TEST("tests fail when requirement errors occur") {
   using caf::test::requirement_failed;
   auto& rep = caf::test::reporter::instance();
   SECTION("require_eq fails when lhs != rhs") {
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_eq(1, 2); });
+      [this] { require_eq(1, 2); });
     require_eq(1, 1);
   }
   SECTION("require_ne fails when lhs == rhs") {
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_ne(1, 1); });
+      [this] { require_ne(1, 1); });
     require_ne(1, 2);
   }
   SECTION("require_le fails when lhs > rhs") {
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_le(2, 1); });
+      [this] { require_le(2, 1); });
     require_le(1, 2);
     require_le(2, 2);
   }
   SECTION("require_lt fails when lhs >= rhs") {
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_lt(2, 2); });
+      [this] { require_lt(2, 2); });
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_lt(2, 1); });
+      [this] { require_lt(2, 1); });
     require_lt(1, 2);
   }
   SECTION("require_ge fails when lhs < rhs") {
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_ge(1, 2); });
+      [this] { require_ge(1, 2); });
     require_ge(2, 1);
     require_ge(2, 2);
   }
   SECTION("require_gt fails when lhs <= rhs") {
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_gt(1, 1); });
+      [this] { require_gt(1, 1); });
     should_fail_with_exception<requirement_failed>(
-      [this]() { require_gt(1, 2); });
+      [this] { require_gt(1, 2); });
     require_gt(2, 1);
   }
   caf::log::test::debug("this test had {} checks", rep.test_stats().total());
@@ -88,13 +126,163 @@ TEST("tests fail when requirement errors occur") {
 
 #endif // CAF_ENABLE_EXCEPTIONS
 
+TEST("all metric checks fail if the metric does not exist") {
+  caf::telemetry::metric_registry reg;
+  current_metric_registry(&reg);
+  metric_registry_poll_interval(1ns);
+  metric_registry_poll_timeout(1ns);
+  should_fail([this] { check_metric_eq("tst", "name", 1); });
+  should_fail([this] { check_metric_eq("tst", "name", {{"a", "b"}}, 1); });
+  should_fail([this] { check_metric_eq("tst", "name", apx1); });
+  should_fail([this] { check_metric_eq("tst", "name", {{"a", "b"}}, apx1); });
+  should_fail([this] { check_metric_ne("tst", "name", 1); });
+  should_fail([this] { check_metric_ne("tst", "name", {{"a", "b"}}, 1); });
+  should_fail([this] { check_metric_ne("tst", "name", apx1); });
+  should_fail([this] { check_metric_ne("tst", "name", {{"a", "b"}}, apx1); });
+  should_fail([this] { check_metric_lt("tst", "name", 1); });
+  should_fail([this] { check_metric_lt("tst", "name", {{"a", "b"}}, 1); });
+  should_fail([this] { check_metric_lt("tst", "name", 1.0); });
+  should_fail([this] { check_metric_lt("tst", "name", {{"a", "b"}}, 1.0); });
+  should_fail([this] { check_metric_le("tst", "name", 1); });
+  should_fail([this] { check_metric_le("tst", "name", {{"a", "b"}}, 1); });
+  should_fail([this] { check_metric_le("tst", "name", 1.0); });
+  should_fail([this] { check_metric_le("tst", "name", {{"a", "b"}}, 1.0); });
+  should_fail([this] { check_metric_gt("tst", "name", 1); });
+  should_fail([this] { check_metric_gt("tst", "name", {{"a", "b"}}, 1); });
+  should_fail([this] { check_metric_gt("tst", "name", 1.0); });
+  should_fail([this] { check_metric_gt("tst", "name", {{"a", "b"}}, 1.0); });
+  should_fail([this] { check_metric_ge("tst", "name", 1); });
+  should_fail([this] { check_metric_ge("tst", "name", {{"a", "b"}}, 1); });
+  should_fail([this] { check_metric_ge("tst", "name", 1.0); });
+  should_fail([this] { check_metric_ge("tst", "name", {{"a", "b"}}, 1.0); });
+}
+
+TEST("metric checks pass if the metric exists with a matching value") {
+  caf::telemetry::metric_registry reg;
+  current_metric_registry(&reg);
+  metric_registry_poll_interval(1ns);
+  metric_registry_poll_timeout(1ns);
+  SECTION("int gauge with no labels") {
+    reg.gauge_singleton("tst", "name", "test")->inc(7);
+    check_metric_eq("tst", "name", 7);
+    check_metric_ne("tst", "name", 0);
+    check_metric_lt("tst", "name", 8);
+    check_metric_le("tst", "name", 8);
+    check_metric_gt("tst", "name", 6);
+    check_metric_ge("tst", "name", 6);
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "c"}}, 7); });
+  }
+  SECTION("int gauge with labels") {
+    reg.gauge_family("tst", "name", {"a"}, "test")
+      ->get_or_add({{"a", "b"}})
+      ->inc(7);
+    check_metric_eq("tst", "name", {{"a", "b"}}, 7);
+    check_metric_ne("tst", "name", {{"a", "b"}}, 0);
+    check_metric_lt("tst", "name", {{"a", "b"}}, 8);
+    check_metric_le("tst", "name", {{"a", "b"}}, 8);
+    check_metric_gt("tst", "name", {{"a", "b"}}, 6);
+    check_metric_ge("tst", "name", {{"a", "b"}}, 6);
+    should_fail([this] { check_metric_eq("tst", "name", 7); });
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "c"}}, 7); });
+  }
+  SECTION("int counter with no labels") {
+    reg.counter_singleton("tst", "cnt_no_lbl", "test")->inc(7);
+    check_metric_eq("tst", "cnt_no_lbl", 7);
+    check_metric_ne("tst", "cnt_no_lbl", 0);
+    check_metric_lt("tst", "cnt_no_lbl", 8);
+    check_metric_le("tst", "cnt_no_lbl", 8);
+    check_metric_gt("tst", "cnt_no_lbl", 6);
+    check_metric_ge("tst", "cnt_no_lbl", 6);
+    should_fail(
+      [this] { check_metric_eq("tst", "cnt_no_lbl", {{"a", "c"}}, 7); });
+  }
+  SECTION("int counter with labels") {
+    reg.counter_family("tst", "name", {"a"}, "test")
+      ->get_or_add({{"a", "b"}})
+      ->inc(7);
+    check_metric_eq("tst", "name", {{"a", "b"}}, 7);
+    check_metric_ne("tst", "name", {{"a", "b"}}, 0);
+    check_metric_lt("tst", "name", {{"a", "b"}}, 8);
+    check_metric_le("tst", "name", {{"a", "b"}}, 8);
+    check_metric_gt("tst", "name", {{"a", "b"}}, 6);
+    check_metric_ge("tst", "name", {{"a", "b"}}, 6);
+    should_fail([this] { check_metric_eq("tst", "name", 7); });
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "c"}}, 7); });
+  }
+  SECTION("double gauge with no labels") {
+    reg.gauge_singleton<double>("tst", "name", "test")->inc(7.0);
+    check_metric_eq("tst", "name", apx7);
+    check_metric_ne("tst", "name", apx1);
+    check_metric_lt("tst", "name", 8.0);
+    check_metric_le("tst", "name", 8.0);
+    check_metric_gt("tst", "name", 6.0);
+    check_metric_ge("tst", "name", 6.0);
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "c"}}, apx7); });
+  }
+  SECTION("double gauge with labels") {
+    reg.gauge_family<double>("tst", "name", {"a"}, "test")
+      ->get_or_add({{"a", "b"}})
+      ->inc(7.0);
+    check_metric_eq("tst", "name", {{"a", "b"}}, apx7);
+    check_metric_ne("tst", "name", {{"a", "b"}}, apx1);
+    check_metric_lt("tst", "name", {{"a", "b"}}, 8.0);
+    check_metric_le("tst", "name", {{"a", "b"}}, 8.0);
+    check_metric_gt("tst", "name", {{"a", "b"}}, 6.0);
+    check_metric_ge("tst", "name", {{"a", "b"}}, 6.0);
+    should_fail([this] { check_metric_eq("tst", "name", apx7); });
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "c"}}, apx7); });
+  }
+  SECTION("double counter with no labels") {
+    reg.counter_singleton<double>("tst", "name", "test")->inc(7.0);
+    check_metric_eq("tst", "name", apx7);
+    check_metric_ne("tst", "name", apx1);
+    check_metric_lt("tst", "name", 8.0);
+    check_metric_le("tst", "name", 8.0);
+    check_metric_gt("tst", "name", 6.0);
+    check_metric_ge("tst", "name", 6.0);
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "c"}}, apx7); });
+  }
+  SECTION("double counter with labels") {
+    reg.counter_family<double>("tst", "name", {"a"}, "test")
+      ->get_or_add({{"a", "b"}})
+      ->inc(7.0);
+    check_metric_eq("tst", "name", {{"a", "b"}}, apx7);
+    check_metric_ne("tst", "name", {{"a", "b"}}, apx1);
+    check_metric_lt("tst", "name", {{"a", "b"}}, 8.0);
+    check_metric_le("tst", "name", {{"a", "b"}}, 8.0);
+    check_metric_gt("tst", "name", {{"a", "b"}}, 6.0);
+    check_metric_ge("tst", "name", {{"a", "b"}}, 6.0);
+    should_fail([this] { check_metric_eq("tst", "name", apx7); });
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "c"}}, apx7); });
+  }
+  SECTION("approx: double gauge with no labels") {
+    reg.gauge_singleton<double>("tst", "name", "test")->inc(7.0);
+    check_metric_eq("tst", "name", apx7);
+    should_fail([this] { check_metric_eq("tst", "name", apx1); });
+  }
+  SECTION("approx: double gauge with labels") {
+    reg.gauge_family<double>("tst", "name", {"a"}, "test")
+      ->get_or_add({{"a", "b"}})
+      ->inc(7.0);
+    check_metric_eq("tst", "name", {{"a", "b"}}, apx7);
+    should_fail([this] { check_metric_eq("tst", "name", {{"a", "b"}}, apx1); });
+  }
+}
+
 TEST("failed checks increment the failed counter") {
-  check_eq(1, 2);
-  auto stats = caf::test::reporter::instance().test_stats();
-  check_eq(stats.passed, 0u);
-  check_eq(stats.failed, 1u);
-  caf::log::test::debug("reset error count to not fail the test");
-  caf::test::reporter::instance().test_stats({2, 0});
+  auto& rep = caf::test::reporter::instance();
+  auto lvl = rep.verbosity(caf::log::level::quiet);
+  auto before = rep.test_stats();
+  {
+    caf::detail::scope_guard lvl_guard([&rep, lvl]() noexcept { //
+      rep.verbosity(lvl);
+    });
+    check_eq(1, 2);
+  }
+  auto after = rep.test_stats();
+  rep.test_stats(before);
+  check_eq(before.passed, after.passed);
+  check_eq(before.failed + 1, after.failed);
 }
 
 TEST("each run starts with fresh local variables") {
