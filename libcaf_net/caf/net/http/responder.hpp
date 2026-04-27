2c63a6b64988f2a50cb2f3313c76f69ada792426
Zheng Ping <kytexzy@gmail.com>
2026-04-28 00:04:57 +0800

    Update libcaf_net/caf/net/http/responder.hpp
    Co-authored-by: Dominik Charousset <dominik@charousset.de>

--- libcaf_net/caf/net/http/responder.hpp
+++ libcaf_net/caf/net/http/responder.hpp
@@ -81,7 +81,7 @@ public:
 
     /// Sends an HTTP response message to the client. Automatically sets the
     /// the `Content-Length` header fields.
-    void respond(status code, caf::unordered_flat_map<std::string, std::string> headers,
+    void respond(status code, std::span<const std::pair<std::string, std::string>> headers,
                  const_byte_span content) {
       impl_->down()->send_response(code, headers, content);
       impl_->set_completed();
